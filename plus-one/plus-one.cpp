68e93e8a3bb5fd3ab9463695cf45733a309cf418
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-05 18:52:41 -0400

    Time: 7 ms (13.11%) | Memory: 8.8 MB (15.19%) - LeetSync

--- plus-one/plus-one.cpp
+++ plus-one/plus-one.cpp
@@ -4,15 +4,12 @@ public:
 
         for (int i = digits.size() - 1; i >= 0; i--){
             digits[i]++;
-            if (digits[i] <= 9){ return digits; }
-            digits[i] = bool(i == 0) * 10; 
+            if (digits[i] == 10){ digits[i] = 0; }
+            else { return digits;}
         }
 
-        if (digits[0] > 9){
-            digits[0] = 1;
-            digits.push_back(0);
-        }
-
+        digits[0] = 1;
+        digits.push_back(0);
         return digits; 
     }
 };
