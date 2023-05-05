c9f36bcc1722b87ac87656b40152ce8c4da27733
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-05 19:09:28 -0400

    Time: 0 ms (100.00%) | Memory: 8.7 MB (63.84%) - LeetSync

--- plus-one/plus-one.cpp
+++ plus-one/plus-one.cpp
@@ -3,9 +3,11 @@ public:
     vector<int> plusOne(vector<int>& digits) {
 
         for (int i = digits.size() - 1; i >= 0; i--){
-            digits[i]++;
-            if (digits[i] == 10){ digits[i] = 0; }
-            else { return digits;}
+            if (digits[i] == 9){ digits[i] = 0; }
+            else { 
+                digits[i]++;
+                return digits;
+            }
         }
 
         digits[0] = 1;
