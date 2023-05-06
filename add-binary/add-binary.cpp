3d7648208e52947eb9dd7b9861552a1e6a96bf88
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-05 20:33:59 -0400

    Time: 20 ms (7.69%) | Memory: 6.5 MB (71.11%) - LeetSync

---
+++ add-binary/add-binary.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    // Requires: '0' <= integer <= '9'
+    inline int ctoi(char integer){
+        return integer - '0';
+    }
+
+    string addBinary(string a, string b) {
+
+        // Very important
+        string output(max(a.size(), b.size()) + 1, '0');
+
+        int oInd = output.size() - 1; 
+        int aInd = a.size() - 1; 
+        int bInd = b.size() - 1; 
+
+        
+        while (oInd >= 1 || aInd >= 0 || bInd >= 0){
+
+            int sum = 0; 
+
+            if (aInd >= 0){ sum += ctoi(a[aInd]); }
+            if (bInd >= 0){ sum += ctoi(b[bInd]); }
+            if (oInd >= 0){ sum += ctoi(output[oInd]); }
+            
+            cout << sum << " " << output << endl;
+
+            if (sum >= 2){
+                output[oInd - 1] = '1';
+            }
+
+            sum = (sum < 2) ? sum : sum - 2; 
+            output[oInd] = '0' + sum;
+
+            oInd--; 
+            aInd--;
+            bInd--; 
+        }
+
+        if (output[0] == '0'){
+            return string(output.begin() + 1, output.end());
+        }
+
+        return output;
+    }
+
+};
