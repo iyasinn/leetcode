995c9fdcade464ce592c3dd48e3678e4c8bf0a47
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-07 22:30:02 -0400

    Time: 7 ms (95.62%) | Memory: 11 MB (27.28%) - LeetSync

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++){
-            for (int j = i + 1; j < nums.size(); j++){
-                if (nums[i] + nums[j] == target){
-                    return {i, j};
-                }
-            }
-        }
-        return {};
-    }
-
-
-
+    
     // O(n^2) solution
     // vector<int> twoSum(vector<int>& nums, int target) {
     //     for (int i = 0; i < nums.size(); i++){
     //         for (int j = i + 1; j < nums.size(); j++){
-    //             if (nums[i] + nums[j] == target) return {i, j};
+    //             if (nums[i] + nums[j] == target){
+    //                 return {i, j};
+    //             }
     //         }
-    //     }   
-    //     return {0};
-    // }
-    
-    // vector<int> twoSum(vector<int>& nums, int target) {
-        
-    //     // Value and its index
-    //     unordered_map<int, int> output; 
-
-    //     for (int i = 0; i < nums.size(); i++){
-    //         if (output.count(target - nums[i])) return {output[target - nums[i]], i};
-    //         output[nums[i]] = i; 
     //     }
     //     return {};
     // }
+
+    // O(n) time, O(n) space
+    // The big idea here is that we are looking for the target numbers index essentially
+    // Hashmap is storing (number, index) and if target - curr is number, then we got both idnexes
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        // Value and its index
+        unordered_map<int, int> output; 
+
+        for (int i = 0; i < nums.size(); i++){
+             if (output.count(target - nums[i])){
+                    return {output[target - nums[i]], i};
+                }  
+            output[nums[i]] = i; 
+        }
+        return {};
+    }
 };
