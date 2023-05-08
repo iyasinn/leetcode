93bf4de5a3861845ec7e74bab1209c7e6b99613f
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-07 22:11:14 -0400

    Time: 9 ms (69.52%) | Memory: 7.4 MB (57.61%) - LeetSync

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false;
-        int n = s.size(); 
 
-        for (int i = 0; i < n; i++){
+    // Simple array indexing method 
+    // O(n^2) time, O(1) space
+    // Uses a swap method to consider if we've seen something or not ijuhk
+    // bool isAnagram(string s, string t) {
 
-            // This loops searches for s[i] in some t[j], and swaps t[i] with that t[j]
-            for (int j = i; j < n; j++){
-                if (s[i] == t[j]){
-                    swap(t[i], t[j]);
-                    break;
-                }
-            }
+    //     if (s.size() != t.size()) return false;
+    //     int n = s.size(); 
 
-            if (s[i] != t[i]) return false; 
+    //     for (int i = 0; i < n; i++){
+
+    //         // This loops searches for s[i] in some t[j], and swaps t[i] with that t[j]
+    //         for (int j = i; j < n; j++){
+    //             if (s[i] == t[j]){
+    //                 swap(t[i], t[j]);
+    //                 break;
+    //             }
+    //         }
+
+    //         if (s[i] != t[i]) return false; 
+    //     }
+
+    //     return true; 
+    // }
+
+    bool isAnagram(string s, string t){
+
+        if (s.size() != t.size()){ return false; }
+        const int SIZE = s.size();
+
+        unordered_map<char, int> val;
+
+        for (int i = 0; i < SIZE; i++){
+            val[s[i]]++;
+            val[t[i]]--;
+        }
+
+        for (auto v : val){
+            if (v.second != 0){ return false; }
         }
 
         return true; 
