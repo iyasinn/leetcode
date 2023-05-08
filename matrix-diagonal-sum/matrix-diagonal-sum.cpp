26d9110bd5794eec4eba8a8c880485a4194fa90e
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-08 17:32:39 -0400

    Time: 22 ms (5.72%) | Memory: 11.3 MB (64.50%) - LeetSync

--- matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
+    // int diagonalSum(vector<vector<int>>& mat) {
+
+    //     int start = 0; 
+    //     int end = mat[0].size() - 1; 
+    //     int sol = 0; 
+
+    //     for (int row = 0; row < mat.size(); row++){
+    //         sol += mat[row][start];
+    //         sol += mat[row][end];
+    //         if (start == end){ sol -= mat[row][start]; }
+    //         start++; 
+    //         end--; 
+    //     }
+
+    //     return sol;
+    // }
+
     int diagonalSum(vector<vector<int>>& mat) {
 
         int start = 0; 
-        int end = mat[0].size() - 1; 
+        const int numCol = mat[0].size(); 
         int sol = 0; 
 
         for (int row = 0; row < mat.size(); row++){
             sol += mat[row][start];
-            sol += mat[row][end];
-            if (start == end){ sol -= mat[row][start]; }
+            sol += mat[row][numCol - 1 - start];
+            if (start == numCol - 1 - start){ sol -= mat[row][start]; }
             start++; 
-            end--; 
         }
 
         return sol;
     }
+
 };
