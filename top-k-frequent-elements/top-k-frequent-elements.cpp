e940c4ce57425ec7d3bf1af2af8867297e2e4368
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-08 16:01:47 -0400

    Time: 12 ms (89.02%) | Memory: 13.8 MB (25.12%) - LeetSync

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
 
-    // Time: O(n log k ) 
-    // Space: O(n)
+    // // Time: O(n log k ) 
+    // // Space: O(n)
     // vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-    //     // O(n) time, O(n) space
+
     //     unordered_map<int, int> count;
     //     for (auto val : nums){
     //         count[val]++;
     //     }
-        
-    //     // pq works with pairs. Compares first value, then second if there's a tie. 
-    //     // We are creating a min heap, hence the use of std::greater
+
     //     priority_queue<pair<int, int>, 
     //                    vector<pair<int, int>>, 
     //                    std::greater<pair<int,int>>>
     //                    pq;
         
-    //     // O(n log k) time, O(k) space
-    //     // Note we compare first value first, specifically because x.second is the count 
-    //     // inside the hashmap. This is the important value, so we use it
+
     //     for (pair<const int, int>& x : count){
     //         pq.push({x.second, x.first});
     //         if (pq.size() > k){ pq.pop(); }
@@ -29,7 +24,7 @@ public:
     //     vector<int> sol; 
     //     sol.reserve(k);
 
-    //     // O(k) time, O(k) space
+
     //     while (!pq.empty()){
     //         sol.emplace_back(pq.top().second);
     //         pq.pop();
@@ -39,20 +34,44 @@ public:
     // }
 
 
+    // Array method
+    // O(n log n) complexity, does not pass the requirements
+    // vector<int> topKFrequent(vector<int>& nums, int k) {
+
+    //     unordered_map<int, int> count;
+    //     for (auto val : nums) count[val]++;
+
+    //     vector<pair<int, int>> vec; 
+    //     for (pair<const int, int>& p : count){ vec.push_back({p.second, p.first}); }
+
+    //     sort(vec.begin(), vec.end(), greater<pair<int, int>>());
+    //     vector<int> sol(k); 
+    //     transform(vec.begin(), vec.begin() + k, sol.begin(), [](pair<const int, int> p){ return p.second; });
+
+    //     return sol;
+    // }
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
 
         unordered_map<int, int> count;
+        vector<int> solution;
         for (auto val : nums) count[val]++;
-
         vector<pair<int, int>> vec; 
         for (pair<const int, int>& p : count){ vec.push_back({p.second, p.first}); }
-
-        sort(vec.begin(), vec.end(), greater<pair<int, int>>());
-        vector<int> sol(k); 
-        transform(vec.begin(), vec.begin() + k, sol.begin(), [](pair<const int, int> p){ return p.second; });
-
-        return sol;
-
+        std::nth_element(vec.begin(), vec.begin() + k, vec.end(), std::greater<pair<int,int>>());
+        for (int i = 0; i < k; i++){ solution.emplace_back(vec[i].second);}
+        return solution;
     }
 
 };
+
+
+
+
+
+
+
+
+
+
+
