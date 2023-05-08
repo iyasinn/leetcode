6100840de3af4466d0eecffd042262e828f107be
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-08 16:12:54 -0400

    Time: 20 ms (38.29%) | Memory: 13.6 MB (90.25%) - LeetSync

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -64,11 +64,12 @@ public:
     // }
 
     // O(n) solution
+    template<typename T = pair<int, int>>
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> count;
         vector<int> solution;
         for (auto& val : nums) count[val]++;
-        vector<pair<int, int>> vec; 
+        vector<T> vec; 
         for (auto& p : count){ vec.push_back({p.second, p.first}); }
         std::nth_element(vec.begin(), vec.end() - k, vec.end());
         for (int i = vec.size() - k; i < vec.size(); i++){ solution.emplace_back(vec[i].second);}
