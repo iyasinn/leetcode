0b21aae69405dbc8629cd15d8839b8c483199778
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-08 17:41:32 -0400

    Time: 15 ms (56.99%) | Memory: 11.2 MB (91.86%) - LeetSync

--- matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -41,12 +41,13 @@ public:
         const int numCol = mat[0].size(); 
         int sol = 0; 
 
-        for (int row = 0; row < mat.size(); row++){
+        for (int row = 0; row < mat.size() / 2; row++){
             sol += mat[row][row] + mat[row][mat.size() - 1 - row];
+            sol += mat[mat.size() - 1 - row][row] + mat[mat.size() - 1 - row][mat.size() - 1 - row];
         }
 
         if (numCol % 2 == 1){
-            sol -= mat[numCol / 2][numCol / 2];
+            sol += mat[numCol / 2][numCol / 2];
         }
 
         return sol;
