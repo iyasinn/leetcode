1c0405c085d16b125b148e103ec1d9727251cb0c
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-10 00:11:28 -0400

    Time: 52 ms (50.93%) | Memory: 22.7 MB (5.14%) - LeetSync

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = [x.lower() for x in s if x.isalnum()]
-        y = s[:]
+        y = s.copy()
         y.reverse()
         return s == y
 
