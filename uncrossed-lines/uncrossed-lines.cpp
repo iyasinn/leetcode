a0b0a6ce9963b9640270544ac58acb7a7d6384b4
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-11 15:20:20 -0400

    Time: 42 ms (15.47%) | Memory: 13 MB (63.96%) - LeetSync

--- uncrossed-lines/uncrossed-lines.cpp
+++ uncrossed-lines/uncrossed-lines.cpp
@@ -24,34 +24,33 @@ public:
 
 
     // F is the last index of nums 1, S is last index of nums 2  
-    int rec(vector<int>& nums1, vector<int>& nums2, int F, int S, vector<vector<int>>& memo){
+    int rec(vector<int>& n1, vector<int>& n2, int F, int S, vector<vector<int>>& memo){
 
-        if (!(0 <= F && F <= nums1.size()) || !(0 <= S && S <= nums2.size())){
+        if (F < 0 || F >= n1.size() || S < 0 || S >= n2.size()){
             return 0;
         }
-
-        if (memo[F][S] != -1){
+        else if (memo[F][S] != -1){
             return memo[F][S];
         }
 
         int temp = -1; 
 
+        // Check if there's an element we can draw a line to.
+        // Greedily find closest one, then recursively call function
         for (int j = S; j >= 0; j--){
-            if (nums1[F] == nums2[j]){
-                temp = rec(nums1, nums2, F - 1, j - 1, memo) + 1;
+            if (n1[F] == n2[j]){
+                temp = rec(n1, n2, F - 1, j - 1, memo) + 1;
                 break;
             }
         }
 
-        memo[F][S] = max(temp, rec(nums1, nums2, F - 1, S, memo));
+        // Case where we dont draw a line with curr index
+        memo[F][S] = max(temp, rec(n1, n2, F - 1, S, memo));
         return memo[F][S];
     }
 
-
     int maxUncrossedLines(vector<int>& n1, vector<int>& n2) {
-        
         vector<vector<int>> memo(n1.size(), vector<int>(n2.size(), -1));
-
         return rec(n1, n2, n1.size() - 1, n2.size() - 1, memo);
     }
 
