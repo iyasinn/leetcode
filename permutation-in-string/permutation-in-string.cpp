041c9f600e08c5fa4c59bba45efa3f54c5e1d3f5
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-28 02:31:45 -0400

    Time: 11 ms (50.71%) | Memory: 7.2 MB (70.80%) - LeetSync

--- permutation-in-string/permutation-in-string.cpp
+++ permutation-in-string/permutation-in-string.cpp
@@ -44,11 +44,11 @@ public:
 
         if (s2.size() < s1.size()) return false;
 
-        // int letter1[26] = {};
-        // int letter2[26] = {};
+        int letter1[26] = {};
+        int letter2[26] = {};
 
-        array<int, 26> letter1 = {};
-        array<int, 26> letter2 = {};
+        // array<int, 26> letter1 = {};
+        // array<int, 26> letter2 = {};
         int start = 0; 
 
         for (char x : s1){
@@ -60,8 +60,8 @@ public:
             letter2[s2[end] - 'a']++;
             if (end - start + 1 != s1.size()){ continue; }
 
-            // if (equal(std::begin(letter2), std::begin(letter2), std::begin(letter1))){
-            if (letter1 == letter2){
+            if (equal(std::begin(letter1), std::end(letter1), std::begin(letter2))){
+            // if (letter1 == letter2){
                 return true; 
             }
 
