96cbb9abde339624fbe0488a977b1ce4917529fe
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-28 02:09:00 -0400

    Time: 76 ms (17.18%) | Memory: 7.2 MB (70.80%) - LeetSync

---
+++ permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        if (s2.size() < s1.size()) return false;
+
+        int letter[26] = {};
+        int start = 0; 
+
+        for (char x : s1){
+            letter[x - 'a']++;
+        }
+
+        for (int end = 0; end < s2.size(); end++){
+
+            letter[s2[end] - 'a']--;
+            if (end - start + 1 != s1.size()){ continue; }
+
+            bool check = true; 
+            for (auto x : letter){
+                if (x != 0){
+                    check = false;
+                }
+            }
+
+            if (check) return true; 
+
+            cout << start << " " << endl;
+            
+            
+            letter[s2[start] - 'a']++;
+            start++; 
+        }
+
+
+
+        return false; 
+
+        
+    }
+};
