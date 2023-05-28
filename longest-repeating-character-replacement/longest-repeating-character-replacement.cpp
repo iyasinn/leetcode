814c95beb9a97c605d7ad5fc9c51c3e405ead827
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-28 01:29:09 -0400

    Time: 11 ms (68.23%) | Memory: 7 MB (82.40%) - LeetSync

--- longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -5,28 +5,17 @@ public:
     int characterReplacement(string s, int k) {
 
         int alph[26] = {};
-        unordered_map<char, int> letter;
         int best = 0; 
         int start = 0; 
 
         for (int end = 0; end < s.size(); end++){
-
-            // letter[s[end]]++;
             alph[s[end] - 'A']++;
-
-            // int maxCharCount = max_element(letter.begin(), letter.end())->second;
             int maxCharCount = alph[max_element(alph, alph + 26) - begin(alph)];
 
             while (start < end && (end - start + 1) - maxCharCount > k){
                 alph[s[start] - 'A']--;
-                // letter[s[start]]--;
                 start++;
-                // maxCharCount = alph[max_element(alph, alph + 26) - begin(alph)];
-                // maxCharCount = max_element(letter.begin(), letter.end())->second;
             }   
-
-            // cout << start << " " << end << endl;
-
             best = max(best, end - start + 1);
         }
 
