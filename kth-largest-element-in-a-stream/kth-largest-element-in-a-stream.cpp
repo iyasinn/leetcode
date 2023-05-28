a7cc43af8cefae7656d9d0d98893e40951955e86
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-28 17:50:09 -0400

    Time: 38 ms (59.10%) | Memory: 19.7 MB (73.60%) - LeetSync

--- kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,3 +1,36 @@
+// class KthLargest {
+
+// public:
+
+//     // Very similar to median 
+//     KthLargest(int kIn, vector<int>& nums) {
+//         k = kIn; 
+//         for (int val: nums){
+//             add(val);
+//         }
+//     }
+    
+//     int add(int val) {
+        
+//         topK.push(val);
+
+//         if (topK.size() > k){
+//             bottom.push(topK.top());
+//             topK.pop();
+//         }
+
+//         return topK.top();
+//     }
+
+// private: 
+
+//     priority_queue<int, vector<int>, less<int>> bottom; 
+//     priority_queue<int, vector<int>, greater<int>> topK;
+
+//     int k = 0; 
+// };
+
+
 class KthLargest {
 
 public:
@@ -13,9 +46,7 @@ public:
     int add(int val) {
         
         topK.push(val);
-
         if (topK.size() > k){
-            bottom.push(topK.top());
             topK.pop();
         }
         
@@ -24,11 +55,11 @@ public:
 
 private: 
 
-    priority_queue<int, vector<int>, less<int>> bottom; 
     priority_queue<int, vector<int>, greater<int>> topK;
 
     int k = 0; 
 };
+
 
 /**
  * Your KthLargest object will be instantiated and called as such:
