22deb00c0ae63c19e8b168471487b515e86acbff
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-27 22:04:04 -0400

    Time: 6 ms (49.73%) | Memory: 7.4 MB (97.02%) - LeetSync

--- stone-game/stone-game.cpp
+++ stone-game/stone-game.cpp
@@ -2,36 +2,42 @@ class Solution {
 public:
 
 
-    int best(vector<int>& piles, int start, int end, vector<vector<vector<int>>>& memo){
+    // int best(vector<int>& piles, int start, int end, vector<vector<vector<int>>>& memo){
 
-        if (start >= piles.size() || end < 0){
-            return 0; 
-        }
+    //     if (start >= piles.size() || end < 0){
+    //         return 0; 
+    //     }
 
-        bool aliceTurn = ((start - end + 1) % 2) == 0;
+    //     bool aliceTurn = ((start - end + 1) % 2) == 0;
 
-        if (memo[aliceTurn][start][end] != -1){
-            return memo[aliceTurn][start][end];
-        }
+    //     if (memo[aliceTurn][start][end] != -1){
+    //         return memo[aliceTurn][start][end];
+    //     }
 
-        int choseS = best(piles, start + 1, end, memo);
-        int choseE = best(piles, start, end - 1, memo);
+    //     int choseS = best(piles, start + 1, end, memo);
+    //     int choseE = best(piles, start, end - 1, memo);
 
-        if (aliceTurn){
-            choseS += piles[start];
-            choseE += piles[end];
-        }
-        else{
-            choseS -= piles[start];
-            choseE -= piles[end];
-        }
+    //     if (aliceTurn){
+    //         choseS += piles[start];
+    //         choseE += piles[end];
+    //     }
+    //     else{
+    //         choseS -= piles[start];
+    //         choseE -= piles[end];
+    //     }
 
-        return memo[aliceTurn][start][end] = max(choseS, choseE);
-    }
+    //     return memo[aliceTurn][start][end] = max(choseS, choseE);
+    // }
+
+    // bool stoneGame(vector<int>& piles) {
+    //     vector<vector<vector<int>>> memo(2, vector<vector<int>>(piles.size(), vector<int>(piles.size(), -1)));
+    //     int aliceMax = best(piles, 0, piles.size() - 1, memo);
+    //     return aliceMax > 0; 
+    // }
+
 
     bool stoneGame(vector<int>& piles) {
-        vector<vector<vector<int>>> memo(2, vector<vector<int>>(piles.size(), vector<int>(piles.size(), -1)));
-        int aliceMax = best(piles, 0, piles.size() - 1, memo);
-        return aliceMax > 0; 
+        return true; 
     }
+
 };
