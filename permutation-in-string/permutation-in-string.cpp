8e643fd2806d788f9c165a8f58d5b6ae970c0abf
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-28 02:30:43 -0400

    Time: 3 ms (96.63%) | Memory: 7.3 MB (44.80%) - LeetSync

--- permutation-in-string/permutation-in-string.cpp
+++ permutation-in-string/permutation-in-string.cpp
@@ -1,43 +1,78 @@
 class Solution {
 public:
+    // bool checkInclusion(string s1, string s2) {
+
+    //     if (s2.size() < s1.size()) return false;
+
+    //     int letter[26] = {};
+    //     int start = 0; 
+    //     int total = 0; 
+
+    //     for (char x : s1){
+    //         letter[x - 'a']++;
+    //     }
+
+    //     for (int end = 0; end < s2.size(); end++){
+
+    //         letter[s2[end] - 'a']--;
+    //         if (end - start + 1 != s1.size()){ continue; }
+
+    //         bool check = true; 
+    //         for (auto x : letter){
+    //             if (x != 0){
+    //                 check = false;
+    //                 break;
+    //             }
+    //         }
+
+    //         if (check) return true; 
+
+    //         cout << start << " " << endl;
+            
+            
+    //         letter[s2[start] - 'a']++;
+    //         start++; 
+    //     }
+
+
+
+    //     return false;    
+    // }
+
+
     bool checkInclusion(string s1, string s2) {
 
         if (s2.size() < s1.size()) return false;
 
-        int letter[26] = {};
+        // int letter1[26] = {};
+        // int letter2[26] = {};
+
+        array<int, 26> letter1 = {};
+        array<int, 26> letter2 = {};
         int start = 0; 
-        int total = 0; 
 
         for (char x : s1){
-            letter[x - 'a']++;
+            letter1[x - 'a']++;
         }
 
         for (int end = 0; end < s2.size(); end++){
 
-            letter[s2[end] - 'a']--;
+            letter2[s2[end] - 'a']++;
             if (end - start + 1 != s1.size()){ continue; }
 
-            bool check = true; 
-            for (auto x : letter){
-                if (x != 0){
-                    check = false;
-                    break;
-                }
+            // if (equal(std::begin(letter2), std::begin(letter2), std::begin(letter1))){
+            if (letter1 == letter2){
+                return true; 
             }
 
-            if (check) return true; 
-
-            // cout << start << " " << endl;
-            
-            
-            letter[s2[start] - 'a']++;
+            letter2[s2[start] - 'a']--;
             start++; 
         }
 
 
 
-        return false; 
-
-        
+        return false;    
     }
+
+
 };
