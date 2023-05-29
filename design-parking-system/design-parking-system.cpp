6eeb5a1d358280c1eda97d31cfcd07ecc727642c
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-29 16:10:39 -0400

    Time: 74 ms (10.02%) | Memory: 33 MB (88.06%) - LeetSync

--- design-parking-system/design-parking-system.cpp
+++ design-parking-system/design-parking-system.cpp
@@ -1,22 +1,44 @@
+// class ParkingSystem {
+// private: 
+//     int arr[4] = {};
+
+// public:
+//     ParkingSystem(int big, int medium, int small) {
+//         arr[1] = big; 
+//         arr[2] = medium;
+//         arr[3] = small;
+//     }
+    
+//     bool addCar(int carType) {
+//         if (arr[carType] <= 0) return false;
+//         arr[carType]--;
+//         return true; 
+//     }
+
+
+// };
+
+
 class ParkingSystem {
 private: 
-    int arr[4] = {};
+    int arr[3] = {};
 
 public:
     ParkingSystem(int big, int medium, int small) {
-        arr[1] = big; 
-        arr[2] = medium;
-        arr[3] = small;
+        arr[0] = big; 
+        arr[1] = medium;
+        arr[2] = small;
     }
     
     bool addCar(int carType) {
-        if (arr[carType] <= 0) return false;
+        if (arr[--carType] <= 0) return false;
         arr[carType]--;
         return true; 
     }
 
 
 };
+
 
 /**
  * Your ParkingSystem object will be instantiated and called as such:
