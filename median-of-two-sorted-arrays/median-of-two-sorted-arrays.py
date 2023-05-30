9fbb731b80a1adb8b7cc099b744f1e3237534b7b
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-30 17:19:51 -0400

    Time: 106 ms (40.47%) | Memory: 16.6 MB (42.91%) - LeetSync

---
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        join = nums1 + nums2
+        join.sort()
+        SIZE = len(join)
+        if (len(join) % 2 == 0):
+            return (join[SIZE // 2] + join[(SIZE // 2) - 1]) / 2
+        
+        return join[SIZE // 2]
+        
