09e21c9ade0c9515b11563703dbd6fe30f81075f
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-29 23:21:02 -0400

    Time: 4 ms (94.62%) | Memory: 12.6 MB (69.88%) - LeetSync

--- concatenation-of-array/concatenation-of-array.cpp
+++ concatenation-of-array/concatenation-of-array.cpp
@@ -1,37 +1,10 @@
-// class Solution {
-// public:
-//     vector<int> getConcatenation(vector<int>& nums) {
-//         vector<int> ans(nums.size() * 2);
-//         for (int i = 0; i < nums.size(); i++){
-//             ans[i] = ans[nums.size() + i] = nums[i];
-//         }
-//         return ans; 
-//     }
-// };
-
-
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-      
-      vector<int> v;
-      copy(nums.begin(),nums.end(), back_inserter(v));
-      
-      vector<int> store;
-      
-      for(int i = 0 ; i < v.size(); i++)
-      {
-         int data = v[i];
-         store.push_back(data);
-      }
-      
-      for(int i = 0 ; i < v.size(); i++)
-      {
-         int data = v[i];
-         store.push_back(data);
-      }
-      
-      
-      return store;
+        vector<int> ans(nums.size() * 2);
+        for (int i = 0; i < nums.size(); i++){
+            ans[i] = ans[nums.size() + i] = nums[i];
+        }
+        return ans; 
     }
 };
