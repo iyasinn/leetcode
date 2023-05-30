128635a8e35e16029629f36f54d4a0269365d61e
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-29 23:29:24 -0400

    Time: 11 ms (50.11%) | Memory: 10.9 MB (92.73%) - LeetSync

--- left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -1,16 +1,35 @@
+// class Solution {
+// public:
+//     vector<int> leftRightDifference(vector<int>& nums) {
+//         vector<int> ans(nums.size());
+//         // ans contains all the left sums
+//         int left = 0; 
+//         for (int i = 0; i < nums.size(); i++){
+//             ans[i] = left; 
+//             left += nums[i];
+//         }
+
+//         // Now we have all the left sums
+//         // Now lets do the opposite with the right sums, but subtract the right sums 
+//         int right = 0; 
+//         for (int i = nums.size() - 1; i >= 0; i--){
+//             ans[i] = abs(ans[i] - right); 
+//             right += nums[i];
+//         }
+
+//         return ans; 
+//     }
+// };
+
 class Solution {
 public:
     vector<int> leftRightDifference(vector<int>& nums) {
-        vector<int> ans(nums.size());
-        // ans contains all the left sums
-        int left = 0; 
-        for (int i = 0; i < nums.size(); i++){
-            ans[i] = left; 
-            left += nums[i];
+        vector<int> ans(nums.size(), 0);
+
+        for (int i = 1; i < nums.size(); i++){
+            ans[i] = ans[i - 1] + nums[i - 1];
         }
 
-        // Now we have all the left sums
-        // Now lets do the opposite with the right sums, but subtract the right sums 
         int right = 0; 
         for (int i = nums.size() - 1; i >= 0; i--){
             ans[i] = abs(ans[i] - right); 
