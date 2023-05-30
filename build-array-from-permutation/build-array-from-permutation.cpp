be3bc1435fcf8d8f34d96a800ec27359098de619
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-29 23:18:31 -0400

    Time: 17 ms (52.08%) | Memory: 16.5 MB (58.22%) - LeetSync

--- build-array-from-permutation/build-array-from-permutation.cpp
+++ build-array-from-permutation/build-array-from-permutation.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     inline vector<int> buildArray(vector<int>& nums) {
         std::ios_base::sync_with_stdio(false);
+        if(nums.size() == 0){return {}; }
         vector<int> ans(nums.size()); 
         for (int i = 0; i < ans.size(); i++){
             ans[i] = nums[nums[i]];
