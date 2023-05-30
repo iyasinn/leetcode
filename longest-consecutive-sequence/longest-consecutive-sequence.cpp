cbb010a2f29796ca4e469098055d5084d16ee14e
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-30 00:51:49 -0400

    Time: 812 ms (13.01%) | Memory: 60.8 MB (26.25%) - LeetSync

---
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    // O(n^2)
+    // int longestConsecutive(vector<int>& nums) {
+    //     // We would probably need to do multiple runs
+    //     // As we iterate over each element, what info do we get? 
+    //     // We get its start point
+    //     // Everything that is one diff away has the same start point
+    //     // We want O(1) lookup, to determine if specific integers exist, so maybe we could look into a set 
+
+    //     unordered_set<int> n(nums.begin(), nums.end());
+    //     int best = 0; 
+
+    //     for (int i = 0; i < nums.size(); i++){
+
+    //         int count = 1; 
+    //         int curr = nums[i];
+
+    //         while (n.count(++curr)){
+    //             count++;
+    //         }
+
+    //         best = max(best, count);
+    //     }
+
+    //     return best; 
+    // }
+
+    int longestConsecutive(vector<int>& nums) {
+
+        unordered_set<int> n(nums.begin(), nums.end());
+        int best = 0; 
+
+        for (int i = 0; i < nums.size(); i++){
+
+            int count = 1; 
+            int curr = nums[i];
+
+            if (n.count(curr - 1)){
+                continue; 
+            }
+
+            while (n.count(curr + 1)){
+                count++;
+                curr++;
+            }
+
+            best = max(best, count);
+        }
+
+        return best; 
+    }
+};
