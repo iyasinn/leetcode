2ea3afcab13261b1f254feac166a287f5f8f6712
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-05-30 00:34:52 -0400

    Time: 115 ms (98.18%) | Memory: 71.2 MB (25.55%) - LeetSync

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -1,3 +1,11 @@
+
+auto init = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
 class Solution {
 public:
     // bool containsDuplicate(vector<int>& nums) {
