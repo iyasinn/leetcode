4dfe54999a216fec70bec3279ea1256c6014debd
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-02 14:05:55 -0400

    Time: 820 ms (12.87%) | Memory: 60.7 MB (30.68%) - LeetSync

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,57 +1,21 @@
 class Solution {
 public:
-
-    // O(n^2)
-    // int longestConsecutive(vector<int>& nums) {
-    //     // We would probably need to do multiple runs
-    //     // As we iterate over each element, what info do we get? 
-    //     // We get its start point
-    //     // Everything that is one diff away has the same start point
-    //     // We want O(1) lookup, to determine if specific integers exist, so maybe we could look into a set 
-
-    //     unordered_set<int> n(nums.begin(), nums.end());
-    //     int best = 0; 
-
-    //     for (int i = 0; i < nums.size(); i++){
-
-    //         int count = 1; 
-    //         int curr = nums[i];
-
-    //         while (n.count(++curr)){
-    //             count++;
-    //         }
-
-    //         best = max(best, count);
-    //     }
-
-    //     return best; 
-    // }
-
-
-    //THINK ABOUT PROPERTIES
-    // THINK ABOUT SEQUNcCES OF NUMBERS 
-    // AND WHAT PROPERTIES THEY MAY HAVE 
-
     int longestConsecutive(vector<int>& nums) {
-        if (nums.empty()) return 0; 
-        unordered_set<int> n(nums.begin(), nums.end());
-        int best = 1; 
+        
+        // Duplicates dont matter
+        unordered_set<int> mp(nums.begin(), nums.end());
+        int best = 0; 
 
-        for (int i = 0; i < nums.size(); i++){
-
-            int count = 1; 
-            int curr = nums[i];
-
-            if (n.count(curr - 1) || !n.count(curr + 1)){
+        for (int n : nums){
+            if (mp.count(n - 1)){
                 continue; 
             }
-
-            while (n.count(curr + 1)){
-                count++;
+            int curr = 0; 
+            while (mp.count(n)){
+                n++; 
                 curr++;
             }
-
-            best = max(best, count);
+            best = max(best, curr);
         }
 
         return best; 
