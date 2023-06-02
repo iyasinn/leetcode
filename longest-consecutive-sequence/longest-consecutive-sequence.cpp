c9db0ab4c33d9c876202699036b0096fb11376c3
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-02 14:11:31 -0400

    Time: 178 ms (57.88%) | Memory: 60.9 MB (28.51%) - LeetSync

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,22 +1,66 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
+    // int longestConsecutive(vector<int>& nums) {
     
-        unordered_set<int> mp(nums.begin(), nums.end());
-        int best = 0; 
+    //     unordered_set<int> mp(nums.begin(), nums.end());
+    //     int best = 0; 
 
-        for (int& n : nums){
-            if (mp.count(n - 1)){
-                continue; 
+    //     for (int& n : nums){
+    //         if (mp.count(n - 1)){
+    //             continue; 
+    //         }
+    //         int curr = 0; 
+    //         while (mp.count(n)){
+    //             n++; 
+    //             curr++;
+    //         }
+    //         best = max(best, curr);
+    //     }
+
+    //     return best; 
+    // }
+
+
+    // int longestConsecutive(vector<int>& nums) {        
+    //     sort(nums.begin(), nums.end());
+    //     if(nums.size()==0) return 0;
+    //     int maxcounter=0, counter=1;
+    //     for(int i=1;i<nums.size();i++){
+    //         if(nums[i]==nums[i-1]+1){
+    //            counter++;
+    //         }
+    //         else if(nums[i]!=nums[i-1]) {
+    //             maxcounter = max(maxcounter, counter);
+    //             counter=1;
+    //         }
+    //     }
+    //     maxcounter = max(maxcounter, counter);
+    //     return maxcounter;
+    // }
+
+
+        int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+        
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int maxCount = 1;
+        
+        for (int num : numSet) {
+            if (numSet.find(num - 1) == numSet.end()) {  // Check if the current element is the start of a subsequence
+                int currNum = num;
+                int count = 1;
+                
+                while (numSet.find(currNum + 1) != numSet.end()) {  // Count consecutive elements
+                    currNum++;
+                    count++;
+                }
+                
+                maxCount = max(maxCount, count);  // Update the maximum count
             }
-            int curr = 0; 
-            while (mp.count(n)){
-                n++; 
-                curr++;
-            }
-            best = max(best, curr);
         }
-
-        return best; 
+        
+        return maxCount;
     }
+
 };
