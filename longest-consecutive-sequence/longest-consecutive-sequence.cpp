abd91de2cede2939a9e4af34f7588c0bf1ca4fa2
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-02 14:12:47 -0400

    Time: 619 ms (25.89%) | Memory: 60.7 MB (31.75%) - LeetSync

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    // int longestConsecutive(vector<int>& nums) {
+    int longestConsecutive(vector<int>& nums) {
     
-    //     unordered_set<int> mp(nums.begin(), nums.end());
-    //     int best = 0; 
+        unordered_set<int> mp(nums.begin(), nums.end());
+        int best = 0; 
 
-    //     for (int& n : nums){
-    //         if (mp.count(n - 1)){
-    //             continue; 
-    //         }
-    //         int curr = 0; 
-    //         while (mp.count(n)){
-    //             n++; 
-    //             curr++;
-    //         }
-    //         best = max(best, curr);
-    //     }
+        for (int& n : nums){
+            if (mp.find(n - 1) != mp.end()){
+                continue; 
+            }
+            int curr = 0; 
+            while (mp.find(n) != mp.end()){
+                n++; 
+                curr++;
+            }
+            best = max(best, curr);
+        }
 
-    //     return best; 
-    // }
+        return best; 
+    }
 
 
     // int longestConsecutive(vector<int>& nums) {        
@@ -39,28 +39,28 @@ public:
     // }
 
 
-        int longestConsecutive(vector<int>& nums) {
-        if (nums.size() == 0)
-            return 0;
+    //     int longestConsecutive(vector<int>& nums) {
+    //     if (nums.size() == 0)
+    //         return 0;
         
-        unordered_set<int> numSet(nums.begin(), nums.end());
-        int maxCount = 1;
+    //     unordered_set<int> numSet(nums.begin(), nums.end());
+    //     int maxCount = 1;
         
-        for (int num : numSet) {
-            if (numSet.find(num - 1) == numSet.end()) {  // Check if the current element is the start of a subsequence
-                int currNum = num;
-                int count = 1;
+    //     for (int num : numSet) {
+    //         if (numSet.find(num - 1) == numSet.end()) {  // Check if the current element is the start of a subsequence
+    //             int currNum = num;
+    //             int count = 1;
                 
-                while (numSet.find(currNum + 1) != numSet.end()) {  // Count consecutive elements
-                    currNum++;
-                    count++;
-                }
+    //             while (numSet.find(currNum + 1) != numSet.end()) {  // Count consecutive elements
+    //                 currNum++;
+    //                 count++;
+    //             }
                 
-                maxCount = max(maxCount, count);  // Update the maximum count
-            }
-        }
+    //             maxCount = max(maxCount, count);  // Update the maximum count
+    //         }
+    //     }
         
-        return maxCount;
-    }
+    //     return maxCount;
+    // }
 
 };
