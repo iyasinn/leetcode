5b504a3dd8b32f777adb6c56dcd826ff7ec2a335
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-08 16:07:26 -0400

    Time: 36 ms (77.35%) | Memory: 18.4 MB (93.94%) - LeetSync

--- group-anagrams/group-anagrams.cpp
+++ group-anagrams/group-anagrams.cpp
@@ -1,16 +1,35 @@
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         map<std::array<int, 26>, vector<string>> mp; 
+//         for (auto&& s : strs){
+//             array<int, 26> arr = {}; 
+//             for (char c : s) arr[c - 'a']++;
+//             mp[arr].push_back(s);
+//         }
+//         vector<vector<string>> sol;
+//         for (auto&& [a, b] : mp){
+//             sol.push_back(b);
+//         }
+//         return sol; 
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<std::array<int, 26>, vector<string>> mp; 
-        for (auto&& s : strs){
-            array<int, 26> arr = {}; 
-            for (char c : s) arr[c - 'a']++;
-            mp[arr].push_back(s);
+        using bin = array<uint8_t, 26>;
+        auto m = map<bin, vector<string>>{};
+        for (auto &&str : strs) {
+            auto cnt = bin{};
+            for (auto ch : str)     
+                ++cnt[ch - 'a'];
+            m[cnt].emplace_back(str);
         }
-        vector<vector<string>> sol;
-        for (auto&& vec : mp){
-            sol.push_back(vec.second);
-        }
-        return sol; 
+        auto res = vector<vector<string>>{};
+        for (auto &&[_, group] : m)
+            res.emplace_back(std::move(group));
+        return res;
     }
 };
