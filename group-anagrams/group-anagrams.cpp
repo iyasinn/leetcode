65b5755deded0e84a158216b433b5e7164c90d36
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-08 16:05:33 -0400

    Time: 36 ms (77.35%) | Memory: 21.5 MB (26.56%) - LeetSync

--- group-anagrams/group-anagrams.cpp
+++ group-anagrams/group-anagrams.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<std::array<int, 26>, vector<string>> mp; 
-        for (auto& s : strs){
+        for (auto&& s : strs){
             array<int, 26> arr = {}; 
             for (char c : s) arr[c - 'a']++;
             mp[arr].push_back(s);
