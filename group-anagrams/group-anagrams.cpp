82c7161131df794b5fe92cbd3fce01a1bbd73fe9
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-08 16:08:37 -0400

    Time: 53 ms (24.72%) | Memory: 21.4 MB (26.93%) - LeetSync

--- group-anagrams/group-anagrams.cpp
+++ group-anagrams/group-anagrams.cpp
@@ -1,35 +1,36 @@
-// class Solution {
-// public:
-//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-//         map<std::array<int, 26>, vector<string>> mp; 
-//         for (auto&& s : strs){
-//             array<int, 26> arr = {}; 
-//             for (char c : s) arr[c - 'a']++;
-//             mp[arr].push_back(s);
-//         }
-//         vector<vector<string>> sol;
-//         for (auto&& [a, b] : mp){
-//             sol.push_back(b);
-//         }
-//         return sol; 
-//     }
-// };
-
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        using bin = array<uint8_t, 26>;
-        auto m = map<bin, vector<string>>{};
-        for (auto &&str : strs) {
-            auto cnt = bin{};
-            for (auto ch : str)     
-                ++cnt[ch - 'a'];
-            m[cnt].emplace_back(str);
+        map<std::array<int, 26>, vector<string>> mp; 
+        for (auto&& s : strs){
+            array<int, 26> arr = {}; 
+            for (char c : s) arr[c - 'a']++;
+            mp[arr].emplace_back(s);
         }
-        auto res = vector<vector<string>>{};
-        for (auto &&[_, group] : m)
-            res.emplace_back(std::move(group));
-        return res;
+        vector<vector<string>> sol;
+        // Structured binding!
+        for (auto&& [a, b] : mp){
+            sol.emplace_back(b);
+        }
+        return sol; 
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         using bin = array<uint8_t, 26>;
+//         auto m = map<bin, vector<string>>{};
+//         for (auto &&str : strs) {
+//             auto cnt = bin{};
+//             for (auto ch : str)     
+//                 ++cnt[ch - 'a'];
+//             m[cnt].emplace_back(str);
+//         }
+//         auto res = vector<vector<string>>{};
+//         for (auto &&[_, group] : m)
+//             res.emplace_back(std::move(group));
+//         return res;
+//     }
+// };
