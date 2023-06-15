dd380ab843a413a8263c0b194c5c8fe3fa242c80
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-15 18:26:15 -0400

    Time: 213 ms (5.99%) | Memory: 55.6 MB (46.60%) - LeetSync

---
+++ daily-temperatures/daily-temperatures.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] dailyTemperatures(int[] t) {
+
+        Stack<Integer> stack = new Stack<>();
+        int[] sols = new int[t.length];
+        for (int i = 0; i < t.length; i++){
+            while (!stack.isEmpty() && t[stack.peek()] < t[i]){
+                sols[stack.peek()] = i - stack.peek();
+                stack.pop();
+            } 
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()){
+            sols[stack.peek()] = 0; 
+            stack.pop();
+        }
+
+
+        return sols;
+    }
+}
