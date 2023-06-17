e196be5cea3406619a4cb6a1f3c0e3311fdc80fc
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-17 04:24:59 -0400

    Time: 40 ms (95.60%) | Memory: 19 MB (39.36%) - LeetSync

--- koko-eating-bananas/koko-eating-bananas.cpp
+++ koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,78 +1,36 @@
 class Solution {
 public:
 
-    // O(2^|max| * |piles|)
-    // int minEatingSpeed(vector<int>& piles, int h) {
 
-    //     int currMax = *max_element(piles.begin(), piles.end());
-
-    //     for (int k = 1; k <= currMax; k++){
-    //         if (checkK(piles, k, h)){
-    //             return k; 
-    //         }
-    //     }
-
-    //     return -1; 
-    //     return currMax; 
-    // }
-
-
-
-    // O(n)
-    inline auto checkK(vector<int>& piles, int k, int h) -> bool {
-
-        int hours = 0; 
-        for (int i = piles.size() - 1; i >= 0; i--){
-            // double val = ceil(piles[i] / k);
-            // int val = (piles[i] + k - 1) / k;
-            // if (val > h || hours + val > h) return false;
-            hours += (piles[i] + k - 1) / k;
+    bool works(vector<int>& piles, int k, int h){
+        double hours = 0; 
+        for (int x : piles){
+            hours += ceil(x * 1.0 / k);
         }
-        return hours <= h;
+        return hours <= h; 
     }
 
-    // O(log(2^|max|) * |piles|) = O(|max| * |piles|)
-    inline int minEatingSpeed(vector<int>& piles, int h) {
+    int minEatingSpeed(vector<int>& piles, int h) {
 
         int start = 1; 
-        int end = 1000000000 + 1;
-        int best = 0; 
+        int end = *max_element(piles.begin(), piles.end()); 
+        int best = end; 
 
-        while (start < end){
+        while (start <= end){
 
-            int mid = start + ((end - start) / 2); 
+            int mid = start + (end - start) / 2;
 
-            if (checkK(piles, mid, h)){
-                best = mid; 
-                end = mid; 
+            cout << mid << endl;
+
+            if (works(piles, mid, h)){
+                best = min(best, mid);
+                end = mid - 1; 
             }
-            else{
+            else {
                 start = mid + 1; 
             }
         }
 
-        return best;
+        return best; 
     }
-
-
-    // int minEatingSpeed(vector<int>& piles, int h) {
-    //     int n(piles.size());
-    //     int lo=1, hi= 1'000'000'000;//최대 스피드;
-    //     while(lo<hi){
-    //         int mid= lo+(hi-lo)/2;
-    //         if(eat(piles,mid)<=h){
-    //             hi=mid;
-    //         }else lo=mid+1;
-    //     }
-    //     return hi;
-    // }
-    // int eat(vector<int> &piles, int speed){
-    //     int sum=0;
-    //     for(int pile:piles){
-    //         // Implicitly rounds up because we are either going to the next integer than dividing down 
-    //         sum+=(pile+speed-1)/speed;
-    //     }
-    //     return sum;
-    // }
-
 };
