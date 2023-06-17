eb2723ef5a5b9e99142c2e6cb0b7877c8542f423
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-17 03:08:56 -0400

    Time: 42 ms (56.76%) | Memory: 27.5 MB (87.42%) - LeetSync

---
+++ binary-search/binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0; 
+        int end = nums.size(); 
+
+        while (start < end){
+            int mid = start + (end - start) / 2; 
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if (nums[mid] > target){
+                end = mid; 
+            }
+            else{
+                start = mid + 1; 
+            }
+        }
+        return -1;
+    }
+};
