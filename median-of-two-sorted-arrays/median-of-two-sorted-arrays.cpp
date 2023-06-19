f2c6b22018cfd6fd8c36303bf6a3c66b8ca2ab55
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-18 20:18:40 -0400

    Time: 45 ms (50.66%) | Memory: 89.3 MB (58.38%) - LeetSync

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
 
 
-    // bool validMedian(vector<int>& n1, vector<int>& n2, int median){ 
-    //     int left = 0; 
-    //     int right = 0; 
-    //     return true; 
-    // }
+    bool validMedian(vector<int>& n1, vector<int>& n2, int median){ 
+        int left = 0; 
+        int right = 0; 
+        return true; 
+    }
 
     double findMedianSortedArrays(vector<int>& n1, vector<int>& n2) {
         
@@ -22,7 +22,7 @@ public:
             // at the max we should say at the last index of where we wnat to include
 
             int add = 0; 
-            
+
             if (i1 < n1.size() && i2 < n2.size()){ 
                median = (n1[i1] < n2[i2]) ? n1[i1++] : n2[i2++];
             }
@@ -45,4 +45,90 @@ public:
     
         return median; 
     }
+
+
+
+
+
+    // bool validMedian(vector<int>& n1, vector<int>& n2, int median){ 
+    //     int left = 0; 
+    //     int right = 0; 
+    //     return true; 
+    // }
+
+    // double findMedianSortedArrays(vector<int>& n1, vector<int>& n2) {
+        
+    //     int totalSize = n1.size() + n2.size(); 
+    //     int left = -1; 
+        
+    //     // These are the next medians to consider in that list
+    //     int i1 = n1.size() / 2; 
+    //     int i2 = n2.size() / 2; 
+    //     double median = 0;
+   
+    //     while (!validMedian(n1, n2, median)){
+            
+    //         if (i1 < n1.size()){
+    //             median = n1[i1++];
+    //         }
+    //         else if (i2 < n2.size()){
+    //             median = n2[i2++];
+    //         }
+
+    //         left++;
+    //     }
+
+    //     if (totalSize % 2 == 0){
+    //         int curr = std::numeric_limits<int>::max(); 
+    //         if (i1 < n1.size()) curr = min(curr, n1[i1]);
+    //         if (i2 < n2.size()) curr = min(curr, n2[i2]);
+    //         median = (median + curr) / 2.0;
+    //     }
+    
+    //     return median; 
+    // }
+
+
+
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
