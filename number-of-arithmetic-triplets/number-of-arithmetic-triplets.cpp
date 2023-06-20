7723fb7537f80abd52848eac8b292a16188e4cd3
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-20 01:09:56 -0400

    Time: 3 ms (90.63%) | Memory: 9.1 MB (21.61%) - LeetSync

--- number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
+++ number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -7,17 +7,50 @@ public:
     // And then we can find some O(n) solution that would let us find the perfect j value 
     // 
 
+    // O(n^3) 
+    // int arithmeticTriplets(vector<int>& nums, int diff) {
+    //     int count = 0; 
+    //     for (int i = 0; i < nums.size(); i++){
+    //         for (int j = i + 1; j < nums.size(); j++){
+    //             for (int k = j + 1; k < nums.size(); k++){
+    //                 if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff){
+    //                     count++; 
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return count; 
+    // }
+
+
     int arithmeticTriplets(vector<int>& nums, int diff) {
+
+        unordered_set<int> s(nums.begin(), nums.end());
+
         int count = 0; 
         for (int i = 0; i < nums.size(); i++){
-            for (int j = i + 1; j < nums.size(); j++){
-                for (int k = j + 1; k < nums.size(); k++){
-                    if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff){
-                        count++; 
-                    }
+            for (int k = i + 2; k < nums.size(); k++){
+                int j = nums[i] + diff; 
+                if (j == nums[k] - diff && s.count(j)){
+                    count++;
                 }
             }
         }
         return count; 
     }
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
