78634584d1612cfe946e973f5f8d1db0dc34a546
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-21 17:38:03 -0400

    Time: 8 ms (70.99%) | Memory: 14.7 MB (97.53%) - LeetSync

--- merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,27 +10,56 @@
  */
 class Solution {
 public:
+    // ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+    //     if (!list1 || !list2) return (list1) ? list1 : list2;
+        
+    //     if (list1->val > list2->val) swap(list1, list2);
+    //     ListNode* head = list1; 
+
+    //     while (list1 && list2){
+    //         // list1 is always smaller
+    //         if (!list1->next || list1->next->val > list2->val){
+    //             ListNode* next = list1->next; 
+    //             list1->next = list2; 
+    //             list1 = next; 
+    //             swap(list1, list2);
+    //         }
+    //         else{
+    //             list1 = list1->next; 
+    //         }
+    //     }
+
+
+    //     return head; 
+    // }
+
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
 
         if (!list1 || !list2) return (list1) ? list1 : list2;
-
+        
         if (list1->val > list2->val) swap(list1, list2);
-        ListNode* head = list1; 
+        ListNode* ans = new ListNode(); 
+        ListNode* curr = ans; 
+
+        // The dummy node makes us always be ONE number behind
 
         while (list1 && list2){
-            // list1 is always smaller
-            if (!list1->next || list1->next->val > list2->val){
-                ListNode* next = list1->next; 
-                list1->next = list2; 
-                list1 = next; 
-                swap(list1, list2);
-            }
-            else{
+            if (list1->val < list2->val){
+                curr->next = list1; 
                 list1 = list1->next; 
             }
+            else{
+                curr->next = list2;
+                list2 = list2->next; 
+            } 
+            curr = curr->next; 
         }
 
-
-        return head; 
+        if (list1) curr->next = list1; 
+        else curr->next = list2; 
+        return ans->next; 
     }
+
+
 };
