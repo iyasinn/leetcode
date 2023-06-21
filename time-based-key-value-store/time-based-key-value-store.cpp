91f7d4a290b4cc5c99a5446374c30996e44a32f0
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-21 13:23:18 -0400

    Time: 403 ms (22.62%) | Memory: 129.8 MB (38.18%) - LeetSync

--- time-based-key-value-store/time-based-key-value-store.cpp
+++ time-based-key-value-store/time-based-key-value-store.cpp
@@ -1,23 +1,32 @@
 class TimeMap {
 public:
 
-    unordered_map<string, map<int, string, greater<int>>> data;
+    unordered_map<string, vector<pair<int, string>>> data;
     TimeMap() {
     }
     
     void set(string key, string value, int timestamp) {
-        data[key][timestamp] = value; 
+        data[key].push_back({timestamp, value}); 
     }
     
     string get(string key, int timestamp) {
 
         if (!data.count(key)) return "";
+        auto& vec = data[key];
 
-        auto& m = data[key];
 
-        auto it = m.lower_bound(timestamp);
-        if (it == m.end()) return "";
-        return it->second; 
+        // auto it = upper_bound(begin(data[key]), end(data[key]), pair<int, string>(timestamp, ""), [](
+        // const pair<int, string>& a, const pair<int, string>& b) { return a.first < b.first; });
+        // return it == data[key].begin() ? "" : prev(it)->second;
+
+        auto it = upper_bound(vec.begin(), vec.end(), pair<int, string>({timestamp, ""}), 
+                              [](const pair<int, string> a, const pair<int, string> b)
+                                {
+                                    return a.first < b.first;
+                                });
+
+        return (it == data[key].begin()) ? "" : prev(it)->second;
+        // return (it != vec.end()) ? it->second : ""; 
             
     }
 };
