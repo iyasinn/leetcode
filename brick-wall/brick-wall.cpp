3d0a47adbe2b2534cadf589ecfc199590e6de7f6
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-06-22 20:53:07 -0400

    Time: 58 ms (27.99%) | Memory: 21.7 MB (52.75%) - LeetSync

--- brick-wall/brick-wall.cpp
+++ brick-wall/brick-wall.cpp
@@ -3,24 +3,17 @@ public:
     int leastBricks(vector<vector<int>>& wall) {
         
         unordered_map<int, int> m;
+        int maxCount = 0; 
 
         for (auto& row : wall){
             int edge = 0; 
             for (int i = 0; i < row.size() - 1; i++){
                 edge += row[i];
                 m[edge]++;
+                maxCount = max(maxCount, m[edge]);
             }
         }
 
-        int maxCount = 0; 
-        for (auto [key, val] : m){
-            maxCount = max(maxCount, val);
-        }
-
         return wall.size() - maxCount; 
-
-
-
-
     }
 };
