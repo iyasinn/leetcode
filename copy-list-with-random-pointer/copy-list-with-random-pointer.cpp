a55832c0e43f7d4f15e3177ce38a28501c8a65f5
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-07-02 19:21:50 -0400

    Time: 7 ms (82.46%) | Memory: 11.3 MB (33.73%) - LeetSync

--- copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -16,32 +16,68 @@ public:
 
 class Solution {
 public:
+    // Node* copyRandomList(Node* head) {
+
+    //     if (!head) { return nullptr; }
+
+    //     unordered_map<Node*, Node*> rel;
+    //     rel[nullptr] = nullptr;
+
+    //     Node* temp = head; 
+
+    //     // Create all the new copies with a mapping
+    //     while (temp){
+    //         rel[temp] = new Node(temp->val);
+    //         temp = temp->next;
+    //     }
+
+    //     temp = head;
+
+    //     // Link the next and random values of the new copies
+    //     while (temp){
+    //         rel[temp]->next = rel[temp->next];
+    //         rel[temp]->random = rel[temp->random];
+    //         temp = temp->next;
+    //     }
+
+    //     return rel[head];
+    // }
+
     Node* copyRandomList(Node* head) {
 
+        // This solution wont work because youc ant modify the original linked list, which is kind of obvious :(
+        // Maybe we can unmodify if though
         if (!head) { return nullptr; }
 
-        unordered_map<Node*, Node*> rel;
-        rel[nullptr] = nullptr;
-
-        Node* temp = head; 
-
-        // Create all the new copies with a mapping
-        while (temp){
-            rel[temp] = new Node(temp->val);
-            temp = temp->next;
+        // Creating zigzag merged list
+        // Unique method of merging a list
+        for (Node* curr = head; curr != nullptr;){
+            Node* temp = curr->next; 
+            curr->next = new Node(curr->val);
+            curr->next->next = temp;
+            curr = temp; 
         }
 
-        temp = head;
-
-        // Link the next and random values of the new copies
-        while (temp){
-            rel[temp]->next = rel[temp->next];
-            rel[temp]->random = rel[temp->random];
-            temp = temp->next;
+        // Setting the randoms
+        for (Node* curr = head; curr != nullptr;){
+            Node* copy = curr->next; 
+            copy->random = (curr->random) ? curr->random->next : nullptr; 
+            curr = curr->next->next; 
         }
 
-        return rel[head];
+        Node* sol = head->next; 
+
+        // // Fixing the next connections
+        for (Node* curr = head; curr != nullptr;){
+            Node* copy = curr->next; 
+            curr->next = curr->next->next; 
+            copy->next = (curr->next) ? curr->next->next : nullptr; 
+            curr = curr->next; 
+        }
+
+        return sol;
     }
+
 
 };
 
