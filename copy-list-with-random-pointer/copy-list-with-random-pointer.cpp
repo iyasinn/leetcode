f4593fc44b2e1671ac40b379d56f242e9ad79452
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-07-02 18:39:15 -0400

    Time: 12 ms (41.98%) | Memory: 11.2 MB (61.66%) - LeetSync

--- copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,57 +17,32 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        /*
-            if (!head) return;
-
-            create mapping
-            create tempHead;
-            mapping.insert(null -> null) 
-
-            create walkTemp
-            create walkHead
-
-            while (walkHead) 
-                our temphead next will point to a new list with the same value as walk head
-                we will map our walkHead to that new walkTemp
-
-            waslkTemp back = to tempHead->next
-            walkHead = head
-
-            while (walkHead)
-                waslkTemp->random = hashmap(walkHead->next)
-        
-        */
 
         if (!head) { return nullptr; }
 
         unordered_map<Node*, Node*> rel;
+        rel[nullptr] = nullptr;
 
-        // dummy head
-        Node* sol = new Node(0);
-        Node* walkSol = sol;
-        Node* walkHead = head; 
+        Node* temp = head; 
 
-        while (walkHead){
-            walkSol->next = new Node(walkHead->val);
-            rel[walkHead] = walkSol->next;
-            walkSol = walkSol->next;
-            walkHead = walkHead->next;
+        // Create all the new copies with a mapping
+        while (temp){
+            rel[temp] = new Node(temp->val);
+            temp = temp->next;
         }
 
-        rel[nullptr] = rel[nullptr];
+        temp = head;
 
-        walkSol = sol->next;
-        walkHead = head;
-
-        while (walkHead){
-            walkSol->random = rel[walkHead->random];
-            walkSol = walkSol->next;
-            walkHead = walkHead->next;
+        // Link the next and random values of the new copies
+        while (temp){
+            rel[temp]->next = rel[temp->next];
+            rel[temp]->random = rel[temp->random];
+            temp = temp->next;
         }
 
-        return sol->next;
+        return rel[head];
     }
+
 };
 
 
