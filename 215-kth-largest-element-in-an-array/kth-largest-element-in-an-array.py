cc23b27de3c8e67920c719a10d6d0583a19fb9db
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-07-27 03:52:21 -0400

    Time: 589 ms (27.43%) | Memory: 29.4 MB (86.79%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -9,9 +9,9 @@ class Solution:
     # Find frequency in O(n). Then put in buckets 
     # Then go backwards in O(k) time to find the kth largest element
     # We will do both implementations
-    # Actually bucket sort will not work because we need the buckets to be the value of the elements rather than frequency, and that cause sissus because that would hav eexponential siz ecompleixty
+    # Actually bucket sort will not work because we need the buckets to be the value of the elements rather than frequency, and that cause sissus because
 
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest1(self, nums: List[int], k: int) -> int:
 
         maxHeap = [-1 * element for element in nums]
         # This is a min heap, we need a max heap using heapify
@@ -23,4 +23,56 @@ class Solution:
         
         return heapq.heappop(maxHeap) * -1
 
-    
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        frequency = defaultdict(int)
+        maxVal = nums[0]
+        minVal = nums[0]
+
+        for n in nums: 
+            maxVal = max(maxVal, n)
+            minVal = min(minVal, n)
+            frequency[n] += 1
+        
+        diff = (minVal * -1) if minVal < 0 else 0
+
+        bucket = [0 for _ in range(maxVal + diff + 1)]
+
+        for key, val in frequency.items(): 
+            bucket[key + diff] = val 
+
+        print(bucket)
+        print(diff)
+
+        # Index is the value
+        # i = diff gives you the actual value
+        for i in range(len(bucket) - 1, -1, -1):
+            
+            if (bucket[i] >= k):
+                return i - diff
+
+            k -= bucket[i]
+        
+        return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
