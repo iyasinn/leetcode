1aeb18bb67aa7ed75791145ddff02b01e64541ed
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-07-29 20:59:32 -0400

    Time: 1498 ms (14.97%) | Memory: 40.1 MB (5.43%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -16,15 +16,15 @@ class Solution:
         maxHeap = [-1 * element for element in nums]
         # This is a min heap, we need a max heap using heapify
         # Inverting to get a max heap 
-        heapq.heapify(maxHeap)
+        # heapq.heapify(maxHeap)
 
         for _ in range(k - 1):
             heapq.heappop(maxHeap)
         
         return heapq.heappop(maxHeap) * -1
 
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-
+    def findKthLargest2(self, nums: List[int], k: int) -> int:
+        print(nums)
         maxVal = nums[0]
         minVal = nums[0]
 
@@ -38,6 +38,8 @@ class Solution:
         for num in nums: 
             bucket[num - minVal] += 1 
 
+        # print(bucket)
+
         for i in range(len(bucket) - 1, -1, -1):
             if (bucket[i] >= k):
                 return i + minVal
@@ -45,24 +47,33 @@ class Solution:
         
         return -1
 
+        # Sorting is faster in python
+    # Quick select is quick sort but we select half the array
+    # Average case is O(n), worst is O(n^2)
+    def findKthLargest(self, nums: List[int], k: int) -> int:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # [l, r] inclusive
+        def quickSelect(l, r): 
             
+            p = l
+            pivot = nums[r]
+            
+            # Quickselect is just a greedy selection algorithm
+            for i in range(l, r): 
+                if (nums[i] <= pivot): 
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p += 1
+            # p is at a point where it is no longer <= 1, if it was, we would have continue
+            nums[p], nums[r] = nums[r], nums[p]
+            if (p < k): 
+                return quickSelect(p + 1, r)
+            elif (p > k):
+                return quickSelect(l, p - 1)
+            return nums[p]
+
+        l = 0 
+        r = len(nums) - 1
+
+        # When dealing with positions quesitons, look for iconverting ranks into indexes
+        k = len(nums) - k 
+        return quickSelect(0, len(nums) - 1)
