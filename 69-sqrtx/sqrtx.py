368e2300501c93e3504fe9259487829d44d2c360
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-07-30 01:43:03 -0400

    Time: 1281 ms (24.41%) | Memory: 16.1 MB (99.32%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,9 +1,14 @@
 class Solution:
+    # ALWAYS DO THE BRUTE FORCE 
+    # THE BRUTE FORCE TEACHES YOU HOW TO DO THE ACTUAL SOLUTION
+    # Looking for a number, you do SOLUTION SAPCE SEARCH 
+    
     def mySqrt(self, x: int) -> int:
+
         root = 1 
         while (root * root < x): 
             root += 1
-            
+
         if (root * root == x): 
             return root
 
