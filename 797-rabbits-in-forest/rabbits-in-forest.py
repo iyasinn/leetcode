e679db32f09279028fdaef520931c5c3fdd2375c
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-08-03 23:36:26 -0400

    Time: 46 ms (95.50%) | Memory: 16.4 MB (56.25%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -18,36 +18,36 @@ class Solution:
         
     #     return total
 
-    # def numRabbits(self, answers: List[int]) -> int:
-
-    #     freq = {}
-    #     total = 0
-
-    #     # Essentially we dont wanna see duplicates
-    #     for num in answers: 
-    #         if num in freq and freq[num] > 0: 
-    #             freq[num] -= 1
-    #         else: 
-    #             freq[num] = num 
-    #             total += num + 1
-    
-
-    #     return total
-
-
+    # Fastest in terms of runtime
     def numRabbits(self, answers: List[int]) -> int:
 
         freq = {}
         total = 0
 
+        # Essentially we dont wanna see duplicates
         for num in answers: 
-            freq[num] = freq.get(num, 0) + 1
-
-        print(freq)
-        
-        for key, val in freq.items():
-            total += ceil(val / (key + 1))  * (key + 1)
-         
+            if num in freq and freq[num] > 0: 
+                freq[num] -= 1
+            else: 
+                freq[num] = num 
+                total += num + 1
+    
 
         return total
+
+
+    # Greedy algorithm, from Dev
+    # def numRabbits(self, answers: List[int]) -> int:
+
+    #     freq = {}
+    #     total = 0
+
+    #     for num in answers: 
+    #         freq[num] = freq.get(num, 0) + 1
+
+    #     for key, val in freq.items():
+    #         total += ceil(val / (key + 1))  * (key + 1)
+         
+
+    #     return total
 
