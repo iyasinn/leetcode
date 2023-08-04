0c9b7c6cd3c5cdb0d4c79e39fed94e02890b9cbe
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-08-03 23:35:56 -0400

    Time: 43 ms (97.50%) | Memory: 16.5 MB (17.25%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -18,21 +18,36 @@ class Solution:
         
     #     return total
 
+    # def numRabbits(self, answers: List[int]) -> int:
+
+    #     freq = {}
+    #     total = 0
+
+    #     # Essentially we dont wanna see duplicates
+    #     for num in answers: 
+    #         if num in freq and freq[num] > 0: 
+    #             freq[num] -= 1
+    #         else: 
+    #             freq[num] = num 
+    #             total += num + 1
+    
+
+    #     return total
+
+
     def numRabbits(self, answers: List[int]) -> int:
 
         freq = {}
         total = 0
 
         for num in answers: 
-            if num in freq and freq[num] > 0: 
-                freq[num] -= 1
-            else: 
-                freq[num] = num 
-                total += num + 1
-        
-        print(freq)
-        print(total)
-        
-        return total
+            freq[num] = freq.get(num, 0) + 1
 
+        print(freq)
+        
+        for key, val in freq.items():
+            total += ceil(val / (key + 1))  * (key + 1)
+         
+
+        return total
 
