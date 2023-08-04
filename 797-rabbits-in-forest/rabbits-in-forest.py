e0a8d104f37ce1dc6113d3f74ee97d52b0cb2a0a
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-08-03 21:54:38 -0400

    Time: 39 ms (99.50%) | Memory: 16.4 MB (56.25%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,20 +1,38 @@
 class Solution:
+    # def numRabbits(self, answers: List[int]) -> int:
+
+    #     freq = {}
+    #     total = 0
+
+    #     for num in answers: 
+
+    #         total += 1
+
+    #         if num in freq and freq[num] > 0: 
+    #             freq[num] -= 1
+    #         else: 
+    #             freq[num] = num 
+
+    #     for num in freq.values():
+    #         total += num
+        
+    #     return total
+
     def numRabbits(self, answers: List[int]) -> int:
 
         freq = {}
         total = 0
 
         for num in answers: 
-            
-            total += 1
-
             if num in freq and freq[num] > 0: 
                 freq[num] -= 1
             else: 
                 freq[num] = num 
-
-        for num in freq.values():
-            total += num
+                total += num + 1
+        
+        print(freq)
+        print(total)
         
         return total
+
 
