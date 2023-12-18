d9ab2b4ac90f5ffc2f0d78dfb88e529e39f0f841
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-12-18 05:30:52 -0500

    Time: 159 ms (43.29%) | Memory: 17.8 MB (35.35%) - LeetSync

---
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
@@ -0,0 +1,29 @@
+
+"""
+
+
+observation: Product difference not commutative. diff(A, B) != diff(B, A)
+
+The max difference is most definitely the largest (a * b) subtracted by the smallest (c * d)
+Take the two largest numbers, and the two smallest numbers 
+
+"""
+
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
+
+
+
+        #
+        #|
+        #||
+        #.||
+        #..|| 
+        #...|| 
+        #..||
+        #.||
+        #||
+        #|
+        #
