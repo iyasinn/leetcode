7efbd96abf8f2d24e64568ef3dc39fe8c02001d5
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-12-19 22:21:45 -0500

    Time: 68 ms (39.91%) | Memory: 16.4 MB (23.41%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,8 +1,12 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        prices.sort()
-        if (prices[0] + prices[1]) <= money: 
-            return money - prices[0] - prices[1]
-
-        return money
+        for i, num in enumerate(prices): 
+            if num < prices[1]:
+                prices[i], prices[1] = prices[1], prices[i]
+                i = 1
+            if num < prices[0]: 
+                prices[i], prices[0] = prices[0], prices[i]
         
+        if prices[0] + prices[1] <= money: 
+            return money - prices[0] - prices[1]
+        return money
