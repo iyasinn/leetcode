5d8a675d1fa14fe2750984d6601166810ea6e63e
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-12-22 20:01:31 -0500

    Time: 36 ms (72.53%) | Memory: 17.4 MB (5.49%) - LeetSync

--- 1619-path-crossing/path-crossing.py
+++ 1619-path-crossing/path-crossing.py
@@ -1,22 +1,18 @@
-"""
-We could have a set of all points we've been at. And then deal with that. 
-"""
-
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
         
-        positions = set()
+        visited = set([(0, 0)])
         move = {"N": (0, 1), "S": (0, -1), "E": (1, 0), "W": (-1, 0)}
 
         x = y = 0
-        solution = False
 
         for direction in path: 
-            if solution: 
-                return True
-            positions.add((x, y))
             x += move[direction][0]
             y += move[direction][1]
-            solution = solution or (x, y) in positions
 
-        return solution
+            if (x, y) in visited: 
+                return True
+
+            visited.add((x, y))
+
+        return False
