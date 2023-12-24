1ea83879e71fb912c54d2265d97d7a8a893ccb2c
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-12-23 22:56:18 -0500

    Time: 57 ms (36.55%) | Memory: 17.6 MB (5.67%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,38 +1,38 @@
 def biconditional(a, b, size):
     bitmask = (1 << size) - 1
     result = bin((a ^ b) ^ bitmask)[2:]
-    result = ('0' * (size - len(result))) + result
-    print(bin(a))
-    print(bin(b))
-    print(result)
+    remaining_zeros = size - len(result)
+    result = '0' * remaining_zeros + result
     return result
+
+
+# class Solution:
+#     def minOperations(self, s: str) -> int:
+#         number = int(s, base=2)
+#         num_ones = ceil(len(s) / 2)
+#         start_with_one = (pow(4, num_ones) - 1) // 3
+#         num_ones += -1 if len(s) % 2 == 1 else 0
+#         start_with_zero = 2 * ((pow(4, num_ones) - 1) // 3)
+
+#         diff_zero = biconditional(number, start_with_one, len(s))
+#         diff_one = biconditional(number, start_with_zero, len(s))
+
+#         return min(diff_zero.count('0'), diff_one.count('0')) 
 
 
 class Solution:
     def minOperations(self, s: str) -> int:
-        number = int(s, base=2)
-        # Count for start_one
 
-        num_ones = ceil(len(s) / 2)
-        start_one = (pow(4, num_ones) - 1) // 3
-        num_ones += -1 if len(s) % 2 == 1 else 0
-        start_zero = 2 * ((pow(4, num_ones) - 1) // 3)
+        start_zero = 0
+        start_one = 0
 
-
-        # print(num_ones)
-        # print()
-        # print(bin(number))
-        # print(bin(start_one))
-        # print(bin(start_zero))
-        # print()
-
-        diff_zero = biconditional(number, start_one, len(s))
-        diff_one = biconditional(number, start_zero, len(s))
-
-        # print(bin(number))
-        # print(diff_one)
-        # print(diff_zero)
-
-        # return 3
-        return min(diff_zero.count('0'), diff_one.count('0')) 
+        for i in range(len(s)):
+            if i % 2 == 0: 
+                start_zero += (s[i] != '0')
+                start_one += (s[i] != '1')
+            elif i % 2 == 1: 
+                start_zero += (s[i] != '1')
+                start_one += (s[i] != '0')
+        
+        return min(start_zero, start_one)
 
