585345859f3420670f55e12c90aae8260d156955
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-12-26 06:24:00 -0500

    Time: 271 ms (65.87%) | Memory: 32.5 MB (5.18%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,23 @@
+
+MODULO = ((10 ** 9) + 7)
+
+@cache
+def dp(n, k, target, curr): 
+    if n == 0: 
+        return int(target == curr)
+    elif n > 0 and curr >= target: 
+        return 0
+    
+    ways = 0 
+    for i in range(1, min(k, target - curr) + 1):
+        ways += dp(n - 1, k, target, curr + i)
+        ways % MODULO
+    
+    return ways % MODULO
+
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        return dp(n, k, target, 0) % MODULO
+
+
+        
