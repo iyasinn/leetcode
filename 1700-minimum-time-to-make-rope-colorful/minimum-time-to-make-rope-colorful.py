fcdd8ebeed94a6cbc7a6c6d70bf75954da6e5c44
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2023-12-27 16:20:07 -0500

    Time: 775 ms (99.20%) | Memory: 28.6 MB (5.19%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,39 @@
+# Sliding window and keep the highest to remove balloon in that sliding window
+"""
+We can simply keep a total sum, and keep a largest value. And subtract that largest value from the window when possible
+
+"""
+
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        
+        start = 0 
+        time = 0
+
+        window_time = 0
+        window_max = 0
+
+        for end in range(len(colors)): 
+            
+            if colors[end] == colors[start]: 
+                window_time += neededTime[end]
+                window_max = max(window_max, neededTime[end])
+                continue
+            
+            time += (window_time - window_max)
+            window_time = window_max = neededTime[end]
+            start = end
+
+        print(start, end, window_time, window_max, time)
+        if start < end: 
+            time += (window_time - window_max)
+        
+        return time
+
+
+
+
+            
+                    
+        
