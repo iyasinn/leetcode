46d80ecdae8e381be7c7e48d005546c3983ec568
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-02 12:15:35 -0500

    Time: 108 ms (5.01%) | Memory: 17.4 MB (12.28%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -11,20 +11,52 @@ If n = 1 type of elements, then numRows = n, so O(n) complexity
 if n = distinct n elements, then numRows = 1, so O(n) complexity
 Can it be O(n^2)? I'm not sure
 
+O(n) insertions into the 2d array at the end of the day
+Regardless of the number of rows, it's n elements going in
+So O(n)? 
+
+
+How to make it better? 
+
+We can add to the output array immediately
+We can just need to know which index to push to 
+The hashmap could theoretically store this
+
+
+
+
 """
+
+
 
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        numRows = 0 
-        freq = {}
-        for n in nums: 
-            freq[n] = freq.get(n, 0) + 1
-            numRows = max(numRows, freq[n])
         output = []
-        for _ in range(numRows): 
-            output.append([])
-            for n in freq: 
-                if freq[n] > 0: 
-                    freq[n] -= 1
-                    output[-1].append(n)
+        pos = {}
+
+        for n in nums: 
+            pos[n] = pos.get(n, 0)
+
+            if pos[n] >= len(output): 
+                output.append([])
+            
+            output[pos[n]].append(n)
+            pos[n] += 1
+
         return output
+
+# class Solution:
+#     def findMatrix(self, nums: List[int]) -> List[List[int]]:
+#         numRows = 0 
+#         freq = {}
+#         for n in nums: 
+#             freq[n] = freq.get(n, 0) + 1
+#             numRows = max(numRows, freq[n])
+#         output = []
+#         for _ in range(numRows): 
+#             output.append([])
+#             for n in freq: 
+#                 if freq[n] > 0: 
+#                     freq[n] -= 1
+#                     output[-1].append(n)
+#         return output
