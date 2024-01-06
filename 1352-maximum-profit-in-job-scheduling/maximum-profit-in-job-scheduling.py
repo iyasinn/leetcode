4b8d9fbedd1eeac06059ece14cf62e6b68b2854f
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-06 17:45:19 -0500

    Time: 683 ms (47.70%) | Memory: 97.9 MB (20.49%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -9,9 +9,69 @@ How do we get next intervals in this ordering
 20 -> go thru all remainign ones and make sure next start is after the current end
 
 
+
+---
+
+
+Looks like LIS wherfe start1 <= end1 <= start2 <= end2 I feel 
+
+
+
+
 """
 
 
+
+
+"""
+If we consider every time entry that finishes <= curr_start time
+Then we can simply say that we need start at the beginning of a interval
+And then consider all intervals that come before this beginning interval 
+But also if we have an interval that doesn't intersect, we could just consider it nontrivially
+It depends on start time clearly
+
+How do we get next intervals in this ordering
+20 -> go thru all remainign ones and make sure next start is after the current end
+
+
+"""
+
+
+
+
+# class Solution:
+#     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+#         data = [(startTime[i], endTime[i], profit[i]) for i in range(len(profit))]
+#         data.sort(key=lambda x: (x[1], x[0]))
+
+#         print(data)
+
+#         @lru_cache
+#         def rec(index): 
+#             if index == -1: 
+#                 return 0
+            
+#             curr_start, curr_end, curr_profit = data[index]
+#             max_profit = 0
+
+#             # Need a faster way to find a job that iterating slowly through 
+#             last_element = bisect.bisect_left(data, curr_start, key=lambda x: x[1]) + 1
+            
+#             for i in range(last_element, -1, -1): 
+#                 if i < 0 or i >= len(data): 
+#                     break
+#                 group = data[i]
+#                 start, end, _ = group
+
+#                 if end <= curr_start: 
+#                     max_profit = max(rec(i), max_profit)
+#                 else:
+#                     break
+            
+#             max_profit += curr_profit
+#             return max_profit
+            
+#         return max(rec(i) for i in range(len(data)))
 
 
 class Solution:
