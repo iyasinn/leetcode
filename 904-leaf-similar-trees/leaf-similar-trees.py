7be61241147e970cf2cd5d570e5c441af3a44ab4
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-08 21:43:52 -0500

    Time: 44 ms (28.49%) | Memory: 17.3 MB (28.86%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -5,29 +5,18 @@
 #         self.left = left
 #         self.right = right
 
-def rec(root, seq, remove=False): 
+def rec(root, seq): 
     if root is None: 
         return 
     
-    rec(root.left, seq, remove)
-    rec(root.right, seq, remove)
+    rec(root.left, seq)
+    rec(root.right, seq)
 
     if root.left is None and root.right is None:
-        if remove and len(seq) > 0 and root.val == seq[-1]: 
-            seq.pop()
-        else: 
-            seq.append(root.val)
+        seq.append(root.val)
     
     return seq
 
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        
-        seq = []
-        rec(root1, seq)
-        seq = seq[::-1]
-        rec(root2, seq, True)
-
-        print(seq)
-
-        return len(seq) == 0
+        return rec(root1, []) == rec(root2, [])
