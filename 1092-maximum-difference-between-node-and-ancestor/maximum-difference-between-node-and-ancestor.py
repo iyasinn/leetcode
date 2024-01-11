1b68180fe1df84f46a5adafe9c96f83a398b961b
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-11 02:41:40 -0500

    Time: 37 ms (88.16%) | Memory: 19.6 MB (66.58%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
@@ -12,30 +12,23 @@ Return min, max, diff
 
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        if not root: 
-            return 0
-
         def dfs(root): 
             if not root.left and not root.right: 
                 return root.val, root.val, 0
-
-
-            diff = []
+            diff = 0
             top_min = root.val
             top_max = root.val
 
-            if root.left: 
-                left_min, left_max, left_diff = dfs(root.left)
-                top_max = max(top_max, left_max)
-                top_min = min(top_min, left_min)
-                diff += [abs(root.val - left_min), abs(root.val - left_max), left_diff]
-            if root.right: 
-                right_min, right_max, right_diff = dfs(root.right)
-                top_max = max(top_max, right_max)
-                top_min = min(top_min, right_min)
-                diff += [abs(root.val - right_min), abs(root.val - right_max), right_diff]
+            for subtree in (root.left, root.right): 
+                if not subtree: 
+                    continue
+                tree_min, tree_max, tree_diff = dfs(subtree)
+                top_min = min(top_min, tree_min)
+                top_max = max(top_max, tree_max)
+                diff = max([diff, abs(tree_min - root.val), abs(tree_max - root.val), tree_diff])
 
-
-            return top_min, top_max, max(diff)
+            return top_min, top_max, diff
         
+        if not root: 
+            return 0
         return dfs(root)[2]
