7af65640df6e4a3cb1616c6017e9b6fc381d275e
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-17 01:51:48 -0500

    Time: 38 ms (86.12%) | Memory: 17.5 MB (9.52%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -4,12 +4,5 @@ class Solution:
         for n in arr: 
             freq[n] = freq.get(n, 0) + 1
         
-        bucket = set()
-
-        for key, count in freq.items(): 
-            if count in bucket: 
-                return False 
-            bucket.add(count)
-        
-        return True
+        return len(set(freq.values())) == len(freq.values())
         
