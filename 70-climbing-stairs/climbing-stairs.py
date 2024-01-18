7f1f297fd295e51fc51e4507bdd574c28d706edd
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-18 16:13:55 -0500

    Time: 37 ms (59.00%) | Memory: 17.3 MB (16.28%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,13 +5,11 @@ class Solution:
     # rec(2) = 2
 
     def climbStairs(self, n: int) -> int:
-        if n <= 2: 
-            return n
-        
-        first = 1
-        second = 2
 
-        for _ in range(2, n): 
+        first = 1
+        second = 1
+
+        for _ in range(1, n): 
             first, second = second, first
             second += first
         
