1c567c743c3828921b2096d03dbafd1baf969436
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-19 14:48:07 -0500

    Time: 114 ms (90.47%) | Memory: 17.3 MB (71.22%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -4,7 +4,7 @@ class Solution:
         if len(matrix) == 1: 
             return min(matrix[0])
             
-        dp = [copy.deepcopy(matrix[-2]), copy.deepcopy(matrix[-1])]
+        dp = [matrix[-2], matrix[-1]]
 
         for row in range(len(matrix) - 2, -1, -1): 
             dp[0][0] = matrix[row][0] + min(dp[1][0], dp[1][1])
