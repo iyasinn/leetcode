6d1a4c41717d01f3f645c18d95e4a4f8b705c3d6
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-19 02:45:07 -0500

    Time: 127 ms (68.70%) | Memory: 18.1 MB (61.34%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -7,9 +7,9 @@ class Solution:
             dp[row][0] = matrix[row][0] + dp[row + 1][0]
             dp[row][-1] = matrix[row][-1] + dp[row + 1][-1]
 
-            if len(matrix) > 1: 
-                dp[row][0] = min(dp[row][0], matrix[row][0] + dp[row + 1][1])
-                dp[row][-1] = min(dp[row][-1], matrix[row][-1] + dp[row + 1][-2])
+        
+            dp[row][0] = min(dp[row][0], matrix[row][0] + dp[row + 1][1])
+            dp[row][-1] = min(dp[row][-1], matrix[row][-1] + dp[row + 1][-2])
 
             for col in range(1, len(matrix[row]) - 1):
                 dp[row][col] += min([dp[row + 1][col - 1], 
