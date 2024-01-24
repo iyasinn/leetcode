a4e877a20ffc989b366c58aae412ba57f682cc15
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-23 20:48:16 -0500

    Time: 56 ms (94.07%) | Memory: 17.6 MB (53.98%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.py
+++ 1168-duplicate-zeros/duplicate-zeros.py
@@ -1,27 +1,7 @@
-
-"""
-
-0:00
-Think of the approach 
-1, 0, 2, 3, 0, 4, 5, 0
-We see 0, nothing, 
-we see 5, and since we have at least one shift, we can say 5 jumps to index + shift
-Everything at n - shift is going to dissapear
-Unless But the shift also decreases 
-
-When seeing a 0 we need a move
-Wanna do this in O(1)
-we can consider num zeros
-"""
-
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
-        # So that means every word needs to be move over by 0 
 
-        numZeros = Counter(arr).get(0, 0)
-
-        if arr[-1] == 0: 
-            numZeros -= 1
+        numZeros = Counter(arr[0:len(arr) - 1]).get(0, 0)
         
         for i in range(len(arr) - 2, -1, -1):
             if arr[i] == 0: 
