82e0125180f64783b72aedd180ec7b9813ae8ca2
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-23 20:52:18 -0500

    Time: 50 ms (98.99%) | Memory: 17.5 MB (56.40%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.py
+++ 1168-duplicate-zeros/duplicate-zeros.py
@@ -1,16 +1,15 @@
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
 
-        numZeros = Counter(arr[0:len(arr) - 1]).get(0, 0)
-        
-        for i in range(len(arr) - 2, -1, -1):
-            if arr[i] == 0: 
-                numZeros -= 1
+        numZeros = Counter(arr).get(0, 0)
+
+        for i in range(len(arr) - 1, -1, -1):
+            numZeros -= (arr[i] == 0)
 
             if i + numZeros < len(arr): 
                 arr[i + numZeros] = arr[i]
-
             if arr[i] == 0 and i + numZeros + 1 < len(arr): 
-                arr[i + numZeros + 1] = arr[i]
+                arr[i + numZeros + 1] = 0
+            
  
         
