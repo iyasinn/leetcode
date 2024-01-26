2c539ef22f93be9035da083a81ddc061b915cd5e
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-01-26 15:26:43 -0500

    Time: 54 ms (86.90%) | Memory: 16.6 MB (57.18%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,12 +1,12 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         freq = Counter(nums)
-        bucket = [0] * 101
-        for i in range(len(bucket)):
-            bucket[i] = freq[i - 1] + bucket[i - 1]
+        for i in(range(101)):
+            freq[i] += freq[i - 1]
 
         for i in range(len(nums)): 
-            nums[i] = bucket[nums[i]]
+            nums[i] = freq[nums[i] - 1]
+
         return nums 
 
 
