04a90197d920103f66ee9e3f216ed2cb42e5f8b3
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-02-08 17:16:14 -0500

    Time: 1876 ms (77.17%) | Memory: 30.6 MB (15.03%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.py
@@ -0,0 +1,60 @@
+"""
+0:00
+n^2 is a perfect square
+Potentially we can employ a greedy approach, let's just think about it
+
+Given a number n 
+There exists some largest perfect square that we can subtract from n
+
+n - L
+Then we can now reduce the problem to numSquares(n-L) + 1
+The question is, what can we say about L
+1 <= L <= n^2
+or 
+1 <= L <= n 
+So we need to consider all x^2 from 1 to N and find the largest possible 
+
+When considering of dp 
+
+
+
+The fastest way to get to a number is if it's a perfect square, because if it is, it takes 1 step to get to tha tnumber 
+n - perfect
+closest = rec(n - perfect_square) + 1
+
+
+
+sqrt(n)
+
+
+"""
+
+
+
+
+
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+
+        @cache
+        def rec(n):
+            if n <= 0: 
+                return 0
+            if int(sqrt(n)) == sqrt(n): 
+                return 1
+            
+            best = n    
+
+            for i in range(1, int(sqrt(n)) + 1): 
+
+                r = rec(n - (i * i)) + 1
+                if r < best: 
+                    best = r
+            
+            return best 
+    
+        return rec(n)
+
+
+
