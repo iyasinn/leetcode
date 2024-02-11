85e313825824f596e0d450405008614db5d9fa51
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-02-11 00:01:54 -0500

    Time: 91 ms (50.54%) | Memory: 16.6 MB (73.06%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,10 +1,10 @@
 """
 
+We had an O(n^3) solution
+So we can now work with an O(n^2) solution 
 
 
 """
-
-
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
@@ -26,7 +26,31 @@ class Solution:
                 start -= 1
                 end += 1
 
-        return count
+        return count 
+
+
+# Simple iteration approach
+# class Solution:
+#     def countSubstrings(self, s: str) -> int:
+#         count = 0 
+#         for i, num in enumerate(s): 
+#             start = i - 1 
+#             end = i + 1
+#             count += 1
+
+#             while start >= 0 and end < len(s) and s[start] == s[end]: 
+#                 count += 1
+#                 start -= 1
+#                 end += 1
+
+#             start = i
+#             end = i + 1 
+#             while start >= 0 and end < len(s) and s[start] == s[end]: 
+#                 count += 1
+#                 start -= 1
+#                 end += 1
+
+#         return count
             
                 
 
