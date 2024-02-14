750bc147e61a22db9f8922189f67d4e42208c7b3
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-02-14 17:36:30 -0500

    Time: 1119 ms (19.48%) | Memory: 47.7 MB (57.06%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,8 +1,32 @@
+def next_pos(start, nums):
+    while start < len(nums) and nums[start] < 0: 
+        start += 1
+    return start
+
+def next_neg(start, nums): 
+    while start < len(nums) and nums[start] > 0: 
+        start += 1
+    return start
+    
+
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos = [n for n in nums if n > 0]
-        neg = [n for n in nums if n < 0]
+        pos = next_pos(0, nums)
+        neg = next_neg(0, nums)
+
         output = []
-        for i in range(len(pos)): 
-            output += [pos[i], neg[i]]
+
+        for _ in range(len(nums) // 2): 
+            output += [nums[pos], nums[neg]]
+            pos = next_pos(pos + 1, nums)
+            neg = next_neg(neg + 1, nums)
+
         return output
+
+    # def rearrangeArray(self, nums: List[int]) -> List[int]:
+    #     pos = [n for n in nums if n > 0]
+    #     neg = [n for n in nums if n < 0]
+    #     output = []
+    #     for i in range(len(pos)): 
+    #         output += [pos[i], neg[i]]
+    #     return output
