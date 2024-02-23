6822c9d4a59f728fa755f4c251fdb38080d47b55
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-02-23 18:09:11 -0500

    Time: 597 ms (74.30%) | Memory: 21.7 MB (46.45%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,22 +1,14 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        freq = {num: 0 for num in range(1, n + 1)}
+        freq = [0] * (n + 1)
+        freq[0] = float('inf')
 
         for a, b in trust: 
-            if a in freq: 
-                freq.pop(a)
-            if b in freq: 
-                freq[b] += 1
+            freq[a] = float('-inf')
+            freq[b] += 1
         
-        print(freq)
-
-        found = -1
-
-        for key, val in freq.items(): 
-            if val == n - 1 and found == -1: 
-                found = key 
-            elif val == n - 1: 
-                return False
-        
-        return found
+        if freq.count(n - 1) == 1: 
+            return freq.index(n - 1)
+    
+        return -1
         
