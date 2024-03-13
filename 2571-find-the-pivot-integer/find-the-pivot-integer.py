f70b8ffbd7fc0061d5fc4038514ea97e319d96e9
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-03-13 00:37:01 -0400

    Time: 35 ms (91.35%) | Memory: 16.5 MB (92.38%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,19 +1,41 @@
 # Exponential because iterating with size of integer
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        l, r = 0, n + 1 
-        while l < r: 
-            mid = l + ((r - l) // 2)
-            l_sum = mid * (mid + 1) // 2
-            r_sum = ((n * (n + 1)) // 2) - (mid * (mid - 1) // 2)
-            if l_sum == r_sum: 
-                return mid
-            if l_sum > r_sum: 
-                r = mid 
-            else: 
-                l = mid + 1
-            
-        return -1
+        root = sqrt((n * (n + 1)) // 2)
+        return int(root) if ceil(root) == floor(root) else -1
+
+
+# # Exponential because iterating with size of integer
+# class Solution:
+#     def pivotInteger(self, n: int) -> int:
+#         l, r = 0, n + 1 
+#         while l < r: 
+#             mid = l + ((r - l) // 2)
+#             l_sum = mid * (mid + 1) // 2
+#             r_sum = ((n * (n + 1)) // 2) - (mid * (mid - 1) // 2)
+#             if l_sum == r_sum: 
+#                 return mid
+#             if l_sum > r_sum: 
+#                 r = mid 
+#             else: 
+#                 l = mid + 1
+#         return -1
+
+# Exponential because iterating with size of integer
+# class Solution:
+#     def pivotInteger(self, n: int) -> int:
+#         l, r = 0, n + 1 
+#         while l < r: 
+#             mid = l + ((r - l) // 2)
+#             l_sum = mid * (mid + 1) // 2
+#             r_sum = ((n * (n + 1)) // 2) - (mid * (mid - 1) // 2)
+#             if l_sum == r_sum: 
+#                 return mid
+#             if l_sum > r_sum: 
+#                 r = mid 
+#             else: 
+#                 l = mid + 1
+#         return -1
 
 
 
