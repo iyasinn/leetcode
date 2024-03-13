29389ff0d32a94d605147bc8f3c94434d961c9c8
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-03-13 00:20:56 -0400

    Time: 54 ms (40.73%) | Memory: 16.5 MB (92.38%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,9 +1,10 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        pivot_sum, total = 0, (n * (n + 1)) // 2
-        for pivot in range(1, n + 1):
-            pivot_sum += pivot
-            if pivot_sum == (total - pivot_sum + pivot):
-                return pivot 
+        for p in range(1, n + 1):
+            if (p * (p + 1)) // 2 == (((n * (n + 1)) // 2) - ((p * (p - 1)) // 2)):
+                return p 
         return -1
-        
+
+
+
+
