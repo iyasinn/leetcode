eac44c6712e96f05d35ec9463e396725b83fa146
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-03-13 00:24:48 -0400

    Time: 36 ms (89.46%) | Memory: 16.4 MB (92.38%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,8 +1,18 @@
+# Exponential because iterating with size of integer
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        for p in range(1, n + 1):
-            if (p * (p + 1)) // 2 == (((n * (n + 1)) // 2) - ((p * (p - 1)) // 2)):
-                return p 
+        l, r = 0, n + 1 
+        while l < r: 
+            mid = l + ((r - l) // 2)
+            l_sum = mid * (mid + 1) // 2
+            r_sum = ((n * (n + 1)) // 2) - (mid * (mid - 1) // 2)
+            if l_sum == r_sum: 
+                return mid
+            if l_sum > r_sum: 
+                r = mid 
+            else: 
+                l = mid + 1
+            
         return -1
 
 
