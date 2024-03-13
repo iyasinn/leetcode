df056bdfd2fe625c2760683fa588dbe913ba3600
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-03-13 00:21:12 -0400

    Time: 54 ms (40.73%) | Memory: 16.5 MB (92.38%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -7,4 +7,11 @@ class Solution:
 
 
 
+# class Solution:
+#     def pivotInteger(self, n: int) -> int:
+#         for p in range(1, n + 1):
+#             if (p * (p + 1)) // 2 == (((n * (n + 1)) // 2) - ((p * (p - 1)) // 2)):
+#                 return p 
+#         return -1
+
 
