8c610033395aaff9aceecae9843519b07295530d
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-05-03 00:47:21 -0400

    Time: 34 ms (63.26%) | Memory: 16.5 MB (49.32%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -2,22 +2,16 @@ class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
         v1 = version1.split(".")
         v2 = version2.split(".")
-        # maxLen = max(len(v1), len(v2))
-      
-        # v1 += (maxLen - len(v1)) * ["0"]
-        # v2 += (maxLen - len(v2)) * ["0"]
-        # print(v1)
-        # print(v2)
+        v1 += max(len(v2) - len(v1), 0) * ["0"]
+        v2 += max(len(v1) - len(v2), 0) * ["0"]
 
-        for i in range(max(len(v1), len(v2))):
-            if (i >= len(v1) and int(v2[i]) > 0): 
+        print(v1)
+        print(v2)
+
+        for a, b in zip(v1, v2):
+            if int(a) < int(b): 
                 return -1
-            elif (i >= len(v2) and int(v1[i]) > 0): 
+            elif int(a) > int(b): 
                 return 1
-            elif (i < len(v1) and i < len(v2)): 
-                if int(v1[i]) < int(v2[i]): 
-                    return -1
-                elif int(v1[i]) > int(v2[i]): 
-                    return 1
         
         return 0
