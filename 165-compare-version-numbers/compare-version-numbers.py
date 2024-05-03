316abf6f2924449d6babeec126ca6fbda36fa3ec
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-05-03 00:25:10 -0400

    Time: 39 ms (25.60%) | Memory: 16.4 MB (99.54%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -2,19 +2,22 @@ class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
         v1 = version1.split(".")
         v2 = version2.split(".")
-        maxLen = max(len(v1), len(v2))
-        # v1, v2 = (v2, v1) if len(v1) > len(v2) else (v1, v2)
-        v1 += (maxLen - len(v1)) * ["0"]
-        v2 += (maxLen - len(v2)) * ["0"]
-        print(v1)
-        print(v2)
+        # maxLen = max(len(v1), len(v2))
+      
+        # v1 += (maxLen - len(v1)) * ["0"]
+        # v2 += (maxLen - len(v2)) * ["0"]
+        # print(v1)
+        # print(v2)
 
-        for a, b in zip(v1, v2):
-            if int(a) < int(b): 
+        for i in range(max(len(v1), len(v2))):
+            if (i >= len(v1) and int(v2[i]) > 0): 
                 return -1
-            elif int(a) > int(b):
+            elif (i >= len(v2) and int(v1[i]) > 0): 
                 return 1
+            elif (i < len(v1) and i < len(v2)): 
+                if int(v1[i]) < int(v2[i]): 
+                    return -1
+                elif int(v1[i]) > int(v2[i]): 
+                    return 1
         
-
-
         return 0
