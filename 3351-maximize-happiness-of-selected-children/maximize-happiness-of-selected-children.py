7df96265016012614975024ae5c7baca518d555f
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-05-09 04:23:57 -0400

    Time: 818 ms (42.48%) | Memory: 43.5 MB (60.87%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -0,0 +1,27 @@
+"""
+0:00 - understanding
+choose k children in k turns, where we pick one child per turn 
+
+each pick, all non picked children happiness decreases by 1 until 0 
+
+need maximum sum of selecting k children 
+
+[1, 2, 3]
+k = 2 
+
+it would be wisest to greedily select the largest children
+
+"""
+
+
+
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        total = 0 
+        for i in range(0, k): 
+            total += max(0, happiness[i] - i)
+
+        return total
+        
