f8d6edc9391b63dfe9726dfe4d0d857e3f1ce143
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-05-10 21:30:44 -0400

    Time: 607 ms (62.93%) | Memory: 16.9 MB (66.33%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.py
@@ -1,18 +1,53 @@
 """
+givens
 
-
+generating fractions: O(n^2)
 """
 
 
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        # start with brute force I suppose
-        outputs = []
-        for i in range(len(arr)): 
-            for j in range(i + 1, len(arr)): 
-                outputs.append([arr[i], arr[j]])
-        outputs.sort(key=lambda x: x[0] / x[1])
-        return outputs[k - 1]
+
+        pq = []
+        for i in range(len(arr) - 1): 
+    
+            heapq.heappush(pq, (arr[i] / arr[-1], i, len(arr) - 1))
+        
+
+        counter = 0
+       
+        while len(pq) > 0: 
+
+            val, n, d = pq[0]
+            heapq.heappop(pq)
+            counter += 1
+
+            if counter == k:
+                return [arr[n], arr[d]]
+            elif n == d: 
+                continue 
+
+            d -= 1
+            heapq.heappush(pq, (arr[n] / arr[d], n, d))
+
+        return []
+
+
+
+
+    
+
+
+
+
+# class Solution:
+#     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+#         outputs = []
+#         for i in range(len(arr)): 
+#             for j in range(i + 1, len(arr)): 
+#                 outputs.append([arr[i], arr[j]])
+#         outputs.sort(key=lambda x: x[0] / x[1])
+#         return outputs[k - 1]
 
       
 
