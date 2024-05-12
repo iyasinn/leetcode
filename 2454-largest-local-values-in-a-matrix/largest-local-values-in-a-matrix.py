59b4fdf994b5fd7f9ec7a47c34b878cbf6d56248
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-05-12 00:09:44 -0400

    Time: 94 ms (99.57%) | Memory: 17.1 MB (28.98%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
@@ -4,13 +4,9 @@ class Solution:
         output = []
 
         for row in range(1, len(grid) - 1): 
-
             output.append([])
-
             for col in range(1, len(grid) - 1):
-
-                largest = max(grid[row-1][col-1:col + 2]+grid[row][col-1:col + 2]+grid[row+1][col-1:col+2])
-
+                largest = max(grid[row-1][col-1:col+2]+grid[row][col-1:col+2]+grid[row+1][col-1:col+2])
                 output[-1].append(largest)
 
         print(output)
