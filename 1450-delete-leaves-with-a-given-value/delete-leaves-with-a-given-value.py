6a7237ee61c83909c4b4d0b60ee7746a9cd12395
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-05-16 20:27:44 -0400

    Time: 44 ms (68.14%) | Memory: 17.1 MB (80.39%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -6,20 +6,19 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
-        def helper(root): 
-            if root is None: 
-                return None
-            if root.left is None and root.right is None: 
-                return None if root.val == target else root
-            
-            root.left = helper(root.left)
-            root.right = helper(root.right)
+        if root is None: 
+            return None
+        if root.left is None and root.right is None: 
+            return None if root.val == target else root
+        
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
 
-            if root.left is None and root.right is None and root.val == target: 
-                return None
+        if root.left is None and root.right is None and root.val == target: 
+            return None
 
-            return root 
+        return root 
 
-        return helper(root)
+  
 
         
