7743d84b3db99c1b25d9b1f4c874f52bf53feb6a
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-05-21 21:22:34 -0400

    Time: 7 ms (21.72%) | Memory: 8.3 MB (86.24%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -4,7 +4,6 @@ public:
         int solution = 0;
 
         for (int i = 0; i < (1 << nums.size()); i++){
-            
             uint temp = 0; 
             for (int j = 0; j < nums.size(); j++){
                 if ((1 << j) & i) temp ^= nums[j];
