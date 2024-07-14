c8f13c778ff670d0a652f1157f75b2583c2b9386
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-07-14 19:42:34 -0400

    Time: 44 ms (10.07%) | Memory: 16.6 MB (63.70%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.py
+++ 726-number-of-atoms/number-of-atoms.py
@@ -1,61 +1,51 @@
-
-def isOneQuantity(items, index): 
-    return index + 1 >= len(items) or not items[index + 1].isnumeric()
+def getCount(items, index):
+    if index + 1 >= len(items) or not items[index + 1].isnumeric(): 
+        return 1
+    return int(items[index + 1])
 
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
 
-        simple = [formula[0]]
+        reduced = [formula[0]]
 
         for i in range(1, len(formula)):
             char = formula[i]
-            if simple[-1][0].isupper() and char.islower(): 
-                simple[-1] += char 
-            elif simple[-1][0].isnumeric() and char.isnumeric(): 
-                simple[-1] += char
+            if reduced[-1][0].isupper() and char.islower(): 
+                reduced[-1] += char 
+            elif reduced[-1][0].isnumeric() and char.isnumeric(): 
+                reduced[-1] += char
             else:
-                simple += [char]
+                reduced += [char]
         
-        p = []
+        stack = []
         event = {}
 
-        for i in range(len(simple)): 
-            if simple[i] == "(": 
-                p.append(i)
-                pass
-            elif simple[i] == ")":
-                count = 1 if isOneQuantity(simple, i) else int(simple[i + 1])
-                event[p[-1]] = count
-                event[i] = count
-                p.pop()
-
-        # print(simple)
-        # print(event)
+        for i in range(len(reduced)): 
+            if reduced[i] == "(": 
+                stack.append(i)
+            elif reduced[i] == ")":
+                event[stack[-1]] = getCount(reduced, i)
+                event[i] = getCount(reduced, i)
+                stack.pop()
 
         freq = {}
         mult = 1
 
-        for i in range(len(simple)): 
-            
-            if simple[i].isalpha(): 
-                count = 1 if isOneQuantity(simple, i) else int(simple[i + 1])
-                freq[simple[i]] = freq.get(simple[i], 0) + (count * mult)
-            elif simple[i] == "(": 
+        for i in range(len(reduced)): 
+            if reduced[i].isalpha(): 
+                count = getCount(reduced, i)
+                freq[reduced[i]] = freq.get(reduced[i], 0) + (count * mult)
+            elif reduced[i] == "(": 
                 mult *= event[i]
-            elif simple[i] == ")":
+            elif reduced[i] == ")":
                 mult = mult // event[i]
         
         solution = ""
         for key in sorted(freq.keys()): 
             solution += str(key) + (str(freq[key]) if freq[key] > 1 else "")
+
         return solution
 
-        # multiplier = 1 
-        # counts = {}
-
-        # events = {}
-
-        # for i in range(len(formula)): 
-        #     if events[i] == "(": 
+ 
                 
         
