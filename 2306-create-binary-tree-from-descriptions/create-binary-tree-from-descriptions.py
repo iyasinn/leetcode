40007ef1c53ccd24029adb80f30a3e4b17d56a32
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-07-15 18:00:40 -0400

    Time: 1655 ms (19.48%) | Memory: 26.3 MB (24.35%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
@@ -4,24 +4,46 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+"""
+
+Root has no parent elements
+Each description tells us what can't be the root 
+
+So that means the root is not a child of anything
+So it doesnt appear in child portion
+We also know that all children appear once 
+Or they appear twice 
+
+set of parents - set of children == root 
+but it's a very slow operation 
+
+We could also not do set minus, and jsut consider eac element individually? 
+"""
+
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
 
+
+
         nodes = {}
 
-        root = set(d[0] for d in descriptions)
+        children = set(d[1] for d in descriptions)
+        root = None
         
 
         for d in descriptions: 
             nodes[d[0]] = nodes.get(d[0], TreeNode(d[0]))
             nodes[d[1]] = nodes.get(d[1], TreeNode(d[1]))
 
-            root -= set([d[1]])
+            if d[0] not in children: 
+                root = d[0]
 
             if d[2]: 
                 nodes[d[0]].left = nodes[d[1]]
             else: 
                 nodes[d[0]].right = nodes[d[1]]
-            
         
-        return nodes[list(root)[0]]
+  
+        return nodes[root]
