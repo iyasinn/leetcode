ad5b3781c53c31ad42c9893bf458c92a1ed26399
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-08-09 01:20:08 -0400

    Time: 92 ms (17.65%) | Memory: 17.8 MB (74.63%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.py
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -12,21 +12,20 @@ def getDir(x):
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
 
+        # Both the direction and the magnitude 
         dRow = 1
         dCol = 0
 
+        # Count -> steps for magnitude
         countRow = abs(dRow)
         countCol = abs(dCol)
 
-        total = rows * cols
+        # That when one is active, the other d is 0 and its count is 0
 
+        total = rows * cols
         visited = []
 
-
-
-
         while len(visited) < total: 
-
 
             if (0 <= rStart < rows and 0 <= cStart < cols): 
                 visited.append([rStart, cStart])
@@ -38,9 +37,10 @@ class Solution:
                 rStart += getDir(dCol)
                 countCol -= 1
 
+            # dRow is the current direction moving across 
             if dRow != 0 and countRow == 0: 
                 dCol = dRow
-                countCol = abs(dRow)
+                countCol = abs(dCol)
                 dRow = 0
             elif dCol != 0 and countCol == 0:
                 dRow = (abs(dCol) + 1) * (getDir(dCol) * -1) 
