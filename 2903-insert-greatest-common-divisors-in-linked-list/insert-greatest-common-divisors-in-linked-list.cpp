eb3f362aab6ac3efbbf54918df2f851ef66e6fd7
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-09-09 23:40:59 -0400

    Time: 66 ms (7.57%) | Memory: 35.5 MB (82.75%) - LeetSync

---
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ /*
+    It helps to know the GCD algorithm 
+    I need to understand why this algorithm works 
+ */
+
+
+int getGCD(int a, int b){
+    // a, b = max(a, b), min(a, b);
+
+    if (b == 0){
+        return a;
+    }
+
+    return getGCD(b, a % b);
+}
+
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+
+        auto start = head; 
+
+        while (head && head->next){
+            cout << head->val << endl;
+            cout << head->next->val << endl;
+
+            auto second = head->next; 
+
+            auto temp = new ListNode(getGCD(head->val, head->next->val)); 
+
+            head->next = temp; 
+            temp->next = second; 
+            head = second; 
+        }
+
+        return start; 
+    }
+};
