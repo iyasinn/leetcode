04cfdc76c1fa8c1b5c773353f0552cd75caeec6b
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-01 14:03:01 -0400

    Time: 46 ms (16.32%) | Memory: 33.8 MB (80.54%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,25 +1,27 @@
 #include <numeric> 
 
-bool valid_solution(vector<int>& weights, int days, int capacity){
-    int curr_day = 0, curr_capacity = 0; 
-    for (int i = 0; i < weights.size(); i++){
-        if (weights[i] > capacity){ return false; }
-        if (curr_capacity + weights[i] > capacity){ curr_day += 1; curr_capacity = 0; }
-        curr_capacity += weights[i]; 
-    }
-    curr_day += 1; 
-    return curr_day <= days; 
-}
+
 
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
 
+        auto valid = [&](int capacity){
+            int curr_day = 0, curr_capacity = 0; 
+            for (int i = 0; i < weights.size(); i++){
+                if (weights[i] > capacity){ return false; }
+                if (curr_capacity + weights[i] > capacity){ curr_day += 1; curr_capacity = 0; }
+                curr_capacity += weights[i]; 
+            }
+            curr_day += 1; 
+            return curr_day <= days; 
+        };
+
         int left = 0, right = 1e9; 
 
         while (left < right){
             int mid = (left + right) / 2; 
-            if (valid_solution(weights, days, mid)){
+            if (valid(mid)){
                 right = mid; 
             }
             else{
