dc01d69c0fc002662b54dd889f9368f8f0178644
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-01 17:38:33 -0400

    Time: 132 ms (6.04%) | Memory: 69.4 MB (14.29%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -14,9 +14,10 @@ public:
             return count >= m; 
         };
         int l = 0, r = 1e9; 
+        int solution = -1; 
         while (l < r){
             int mid = l + ((r - l) / 2);
-            if (valid(mid)) { r = mid; }
+            if (valid(mid)) { r = mid; solution = r;  }
             else { l = mid + 1; }
         }
         return valid(r) ? r : -1; 
