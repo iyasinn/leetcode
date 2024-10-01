62c563dcc64246fbfd9a28dc20b1dad146d1aa5f
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-01 17:36:14 -0400

    Time: 150 ms (5.24%) | Memory: 69.3 MB (79.84%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,24 @@
+
+
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        auto valid = [&](int days){
+            int count = 0; 
+            int rolling = 0; 
+            for (int i = 0; i < bloomDay.size(); i++){
+                if (bloomDay[i] <= days) { rolling += 1; } 
+                else { rolling = 0; }
+                if (rolling == k){ count += 1; rolling = 0; }
+            }
+            return count >= m; 
+        };
+        int l = 0, r = 1e9; 
+        while (l < r){
+            int mid = l + ((r - l) / 2);
+            if (valid(mid)) { r = mid; }
+            else { l = mid + 1; }
+        }
+        return valid(r) ? r : -1; 
+    }
+};
