46556910f8647fc8d86a6f44b049ab3853a95970
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-02 12:02:43 -0400

    Time: 229 ms (91.30%) | Memory: 35.8 MB (26.82%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -3,7 +3,3 @@ class Solution:
         copy = sorted(set(arr))
         m = {copy[i]: i + 1 for i in range(len(copy))}
         return [m[val] for val in arr]
-        
-        for i in range(len(arr)): 
-            arr[i] = m[arr[i]]
-        return arr
