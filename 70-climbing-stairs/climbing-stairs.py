547a7ea991529ff39634f7dcb97629103d903c8b
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-02 19:47:38 -0400

    Time: 33 ms (66.63%) | Memory: 16.7 MB (18.41%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,13 +2,16 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1: 
             return 1
-        tab = [0] * n
+
+        tab = [1, 2]
         tab[0] = 1 
         tab[1] = 2; 
 
-        for i in range(2, len(tab)): 
-            tab[i] = tab[i - 1] + tab[i - 2]
+        for i in range(2, n): 
+            x = tab[0] + tab[1]
+            tab[0] = tab[1]
+            tab[1] = x
         
-        return tab[-1]
+        return tab[1]
 
         
