cc0eee5434bbd094d05e15a24a9454719eebff7b
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-02 19:46:10 -0400

    Time: 33 ms (66.63%) | Memory: 16.8 MB (18.41%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,21 +1,14 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        """
-        For eacch stair, we can get to it from previous or prev - 2
-        What gives away this is dp? 
-        T(n) = T(n - 1) + T(n - 2) 
-        DP because we have overlapping subacases
 
-        """
+    @cache
+    def climbStairs(self, n: int) -> int:
         if n == 1: 
             return 1
-        tab = [0] * n
-        tab[0] = 1 
-        tab[1] = 2; 
-
-        for i in range(2, len(tab)): 
-            tab[i] = tab[i - 1] + tab[i - 2]
+        elif n == 2: 
+            return 2
         
-        return tab[-1]
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        
+
 
         
