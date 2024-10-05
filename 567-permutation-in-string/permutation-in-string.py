4fda35c010114e452f4720bfc94a93218206de00
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-05 13:09:27 -0400

    Time: 88 ms (38.32%) | Memory: 16.9 MB (22.10%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq = Counter(s1)
+        curr = Counter(s2[0:len(s1)])
+
+        if freq == curr: 
+            return True
+
+        for i in range(len(s1), len(s2)): 
+            curr[s2[i]] += 1
+            curr[s2[i - len(s1)]] -= 1
+
+            if freq == curr: 
+                return True
+
+
+        return False
+        
+        
