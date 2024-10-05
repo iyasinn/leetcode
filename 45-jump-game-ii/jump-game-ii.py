f1ccb2fa0a7c0ac8ecfb7e85dfd8ca0be390d3ab
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-05 14:11:55 -0400

    Time: 116 ms (34.10%) | Memory: 17.6 MB (80.68%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,26 @@
+# So to get to point d[i], the question is, where can we have come from? 
+# We can go to all dp[i - j] where we coul dhave jumped to those
+# Can we apply our greedy technique here, jump as far right as possible as long as its possible to get there
+# Guaranteed we can get to n - 1
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        curr = 0
+        farth = 0
+        jumps = 0
+
+        for i in range(len(nums)):
+
+            if curr == len(nums) - 1: 
+                break
+
+            farth = max(i + nums[i], farth)
+            farth = min(len(nums) - 1, farth)
+
+            if i == curr: 
+                curr = farth
+                jumps += 1
+        
+        return jumps
+        
