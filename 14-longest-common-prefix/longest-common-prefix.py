858172eba703ccd917e94542c8a360df4d02c9d3
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-06 16:35:26 -0400

    Time: 36 ms (67.72%) | Memory: 16.8 MB (5.82%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,28 +1,21 @@
+
+
+def LCP(a, b): 
+    if len(a) > len(b): 
+        a, b = b, a
+    lcp = ""
+    for i in range(min(len(a), len(b))):
+        if a[i] != b[i]: 
+            break
+        lcp += a[i]
+    return lcp
+
 class Solution:
-
-
-
-
     def longestCommonPrefix(self, strs: List[str]) -> str:
-
-        prefix = "" 
-        firstWord = strs[0]
-
-        for i in range(len(firstWord)):
-
-            letter = firstWord[i]
-
-            for j in range(len(strs)): 
-                word = strs[j]
-                if len(word) <= i or word[i] != letter: 
-                    return prefix 
-                    
-            prefix += letter
-        
-        return prefix
-
-
-            
-
-
+        i = 0
+        done = False
+        lcp = strs[0]
+        for s in strs: 
+            lcp = LCP(lcp, s)
+        return lcp
         
