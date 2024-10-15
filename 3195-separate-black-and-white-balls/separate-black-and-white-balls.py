bf4a624b8037ac2d2e41bb9faec3c1942dbe900f
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-14 23:39:51 -0400

    Time: 121 ms (27.17%) | Memory: 17.4 MB (83.58%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -0,0 +1,26 @@
+# Need all the 1s to the right
+# So this is the distance to the end - 1
+
+"""
+
+
+"""
+
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+
+        steps = 0 
+        blacks = 0
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                continue
+
+            desEnd = len(s) - 1 - blacks
+
+            steps += (desEnd - i)
+
+            blacks += 1
+
+        return steps
+        
