7e2f521bc4f043c5d0724255b3f0a06de17cae4e
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-25 17:24:20 -0400

    Time: 19 ms (97.88%) | Memory: 29.6 MB (92.33%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,72 +1,14 @@
-
-
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
 
-        mainFolders = set(folder)
-        output = []
+        folder.sort()
+        output = [folder[0]]
 
-        for path in folder:
-            old_path = path
-            path = path[1::].split("/")
-            path.pop()
+        for i in range(1, len(folder)):
 
-            parent = ""
-            all_parents = set()
-
-            for item in path: 
-                parent += "/" + item
-                all_parents.add(parent)
-                
-            if len(all_parents - mainFolders) < len(all_parents):
+            parent = output[-1] + "/"
+            if folder[i].startswith(parent):
                 continue
-            
-            output.append(old_path)
-        
+            output.append(folder[i])
+
         return output
-
-
-# class Solution:
-#     def removeSubfolders(self, folder: List[str]) -> List[str]:
-
-#         folder.sort()
-#         output = [folder[0]]
-
-#         for i in range(1, len(folder)):
-
-#             parent = output[-1] + "/"
-#             if folder[i].startswith(parent):
-#                 continue
-#             output.append(folder[i])
-
-#         return output
-                
-
-
-
-      
-
-
-        # mainFolders = set(folder)
-        # output = []
-
-        # for path in folder:
-            
-        #     old_path = path
-        #     path = path[1::].split("/")
-        #     path.pop()
-
-        #     parent = ""
-        #     found = False
-
-        #     for item in path: 
-        #         parent += "/" + item
-        #         if parent in mainFolders:
-        #             found = True
-        #             break
-            
-        #     if not found:
-        #         output.append(old_path)
-        
-        # return output
-
