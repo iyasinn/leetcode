c2d08ae1a009dbd0ba6a3063cb531977970fd67c
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-25 17:19:47 -0400

    Time: 113 ms (69.82%) | Memory: 29.6 MB (80.59%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,44 +1,45 @@
-"""
-This feels like a set problem
 
-everything in the original array is a potential main folder
-
-For each element we can map it to it's parent folders
-and if any of those parent folders appear, we cna delete 
-
-We need a fast way to check subfolder
-I say a subfolder is if you are in th emain set
-Or if you start with a prefix
-
-/a -> {}
-/a/b -> /a 
-/c/d -> /c
-/c/d/e -> /c, /c/d
-"""
-
-def isParent(parent, child):
-    if not parent: 
-        return False
-    for i in range(len(parent)):
-        if i >= len(child) or parent[i] != child[i]:
-            return False
-    return True
 
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
 
-        folder.sort()
-        # The last parent we saw
-        output = [folder[0]]
+        mainFolders = set(folder)
+        output = []
 
-        for i in range(1, len(folder)):
+        for path in folder:
+            old_path = path
+            path = path[1::].split("/")
+            path.pop()
 
-            parent = output[-1] + "/"
-            if folder[i].startswith(parent):
+            parent = ""
+            all_parents = set()
+
+            for item in path: 
+                parent += "/" + item
+                all_parents.add(parent)
+                
+            if len(all_parents - mainFolders) < len(all_parents):
                 continue
-            output.append(folder[i])
-
+            
+            output.append(old_path)
+        
         return output
+
+
+# class Solution:
+#     def removeSubfolders(self, folder: List[str]) -> List[str]:
+
+#         folder.sort()
+#         output = [folder[0]]
+
+#         for i in range(1, len(folder)):
+
+#             parent = output[-1] + "/"
+#             if folder[i].startswith(parent):
+#                 continue
+#             output.append(folder[i])
+
+#         return output
                 
 
 
