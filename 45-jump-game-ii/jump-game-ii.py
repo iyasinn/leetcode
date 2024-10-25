27927777ddca81165c557bbce00ced14d81aab8c
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-24 22:49:20 -0400

    Time: 5 ms (98.79%) | Memory: 17.6 MB (44.11%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,5 +1,28 @@
+"""
+We can keep a farthest value
+And only jump when we reach our current position
+Since it is guaranteed we can reach the end, then it is fine
+"""
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
+
+        jumps = 0
+        l = 0 
+        r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+
+            while l <= r:
+                farthest = max(l + nums[l], farthest)
+                l += 1
+            
+            l = r + 1
+            r = farthest
+            jumps += 1
+
+        return jumps
 
 
         curr = 0
@@ -13,6 +36,7 @@ class Solution:
             if potential == curr:
                 curr = farthest
                 jumps += 1
+            
         
         return jumps
             
