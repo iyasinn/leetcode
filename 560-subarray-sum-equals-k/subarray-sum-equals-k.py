0fba3a4cfe40adf6a0fc308464ef41f195273ae0
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-27 21:27:32 -0400

    Time: 39 ms (36.55%) | Memory: 21.2 MB (5.56%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -5,11 +5,6 @@ class Solution:
         seen = defaultdict(int)
         ans = 0
 
-        # bigP - smaalP = k
-        # smallP = k - bigP
-
-
-
         for prefix in accumulate([0] + nums):
             ans += seen[prefix - k]
             seen[prefix] += 1
