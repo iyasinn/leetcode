a32abb7bf3ccb49f4006eab1c743eb7c01f95e04
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-29 14:39:14 -0400

    Time: 2 ms (9.61%) | Memory: 16.8 MB (7.73%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,29 +1,29 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         
-        temp = []
-        l = 0
-        r = 0 
+        # temp = []
+        # l = 0
+        # r = 0 
 
-        while l < m or r < n:
-            if l < m and r < n:
-                if nums1[l] < nums2[r]:
-                    temp.append(nums1[l])
-                    l += 1
-                else:
-                    temp.append(nums2[r])
-                    r += 1
-                continue
-            if l < m: 
-                temp.append(nums1[l])
-                l += 1
-            else:
-                temp.append(nums2[r])
-                r += 1
+        # while l < m or r < n:
+        #     if l < m and r < n:
+        #         if nums1[l] < nums2[r]:
+        #             temp.append(nums1[l])
+        #             l += 1
+        #         else:
+        #             temp.append(nums2[r])
+        #             r += 1
+        #         continue
+        #     if l < m: 
+        #         temp.append(nums1[l])
+        #         l += 1
+        #     else:
+        #         temp.append(nums2[r])
+        #         r += 1
         
-        for i in range(len(temp)):
-            nums1[i] = temp[i]
-        return nums1
+        # for i in range(len(temp)):
+        #     nums1[i] = temp[i]
+        # return nums1
 
 
 
