db545f35b8e3e9fb6133874bc15965810a2323a4
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-29 03:59:50 -0400

    Time: 195 ms (15.56%) | Memory: 33.8 MB (52.44%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
@@ -5,14 +5,19 @@ class Solution:
         elts = set(nums)
         best = 0
 
+        # O(n)
         for n in nums: 
 
             curr = 1
-            val = n 
+            val = n
+
+            if int(sqrt(val)) in elts and float(int(sqrt(val))) == sqrt(val):
+                continue
 
             while val * val in elts:
                 curr += 1
                 val = val * val
+
             best = max(curr, best)
 
         return best if best > 1 else -1
