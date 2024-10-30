0a200c03be1f071ff38cca5e9e632c2ef818a93c
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-30 13:28:24 -0400

    Time: 0 ms (100.00%) | Memory: 24.5 MB (24.01%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -4,13 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Dont need to go left at a certain point
+
+
+
+
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         if not root:
             return 0
 
         curr = 0
-
+        
         if root.val >= low:
             curr += self.rangeSumBST(root.left, low, high)
         if root.val <= high:
