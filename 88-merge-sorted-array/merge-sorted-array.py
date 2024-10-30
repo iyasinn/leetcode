b23d5e370e2c6313a12c507bd9c51e48ad1d1372
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-30 15:22:45 -0400

    Time: 0 ms (100.00%) | Memory: 16.7 MB (8.19%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -2,16 +2,24 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         
 
-        # Find the n largest elements, and fill the array, then backfill 
+        l, r, i = m - 1, n - 1, len(nums1) - 1
+
+        while r >= 0: 
+            if l >= 0 and nums1[l] > nums2[r]:
+                nums1[i] = nums1[l]
+                l -= 1
+            else:
+                nums1[i] = nums2[r]
+                r -= 1
+            i -= 1
+
+        return nums1 
 
 
-
-        # O(n) two pointer, O(n) space
         l = m - 1
         r = n - 1
 
         i = len(nums1) - 1
-
 
         while l >= 0 and r >= 0: 
             if nums1[l] > nums2[r]:
@@ -33,12 +41,6 @@ class Solution:
             nums1[i] = nums2[r]
             i -= 1
             r -= 1
-
-        # temp.extend(nums1[l:m])
-        # temp.extend(nums2[r:])
-
-        # print(temp)
-        
 
         return nums1
 
