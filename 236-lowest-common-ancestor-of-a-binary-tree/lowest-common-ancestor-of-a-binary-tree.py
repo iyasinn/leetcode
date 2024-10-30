0482581b0d1856cae7f2b99ce1d8acaf45819e10
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-10-30 17:39:29 -0400

    Time: 42 ms (91.97%) | Memory: 21.1 MB (40.33%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -5,71 +5,16 @@
 #         self.left = None
 #         self.right = None
 
-"""
-Simpel approach in head
-Get the path twoards p
-Then get the path towards q. And as you get the path to q, keep updating the common ancesotr you find
-Until you find q
-Because lowest common ancestor will also be the one that is closest to q
-"""
-
-import copy
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # So root is the ancestor, and we find something in left or right
+        if root in [p, q] or (left and right):
+            return root
+        return left or right
         
-        pathP = []
-        currPath = []
-        
-        best = None
-        anc = root
-
-        def dfsP(root):
-            nonlocal pathP
-            nonlocal currPath
-            if not root:
-                return
-            if root == p:
-                pathP = currPath.copy()
-                return
-            
-            for node in [root.left, root.right]:
-                if node:
-                    currPath.append(node)
-                    dfsP(node)
-                    currPath.pop()
-        
-        dfsP(root)
-
-        if q in pathP:
-            return q
-
-        def dfsQ(root):
-            nonlocal anc
-            nonlocal best
-            if not root: 
-                return 
-
-            if root == q:
-                print("ANC")
-                print(anc.val)
-
-                best = anc
-                return
-
-            oldAnc = anc
-            if root in pathP:
-                anc = root
-            
-            dfsQ(root.left)
-            dfsQ(root.right)
-            anc = oldAnc
-
-
-
-        dfsQ(root)
-
-
-
-        return best
-
