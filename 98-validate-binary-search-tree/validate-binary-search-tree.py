a1311a05c179822999249a103b976ced21497dc0
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-02 14:14:03 -0400

    Time: 2 ms (41.41%) | Memory: 18.6 MB (22.88%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -4,21 +4,48 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+"""
+First idea. Just do inorder traversal! 
+"""
+
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
 
-        def dfs(root, lo, hi):
+        prev = float('-inf')
+        valid = True
+
+        def dfs(root):
+            nonlocal prev
+            nonlocal valid
             if root is None: 
                 return True
-            
-            if root.val <= lo or root.val >= hi: 
+
+            left = dfs(root.left)
+
+            if root.val <= prev:
                 return False
-            
-            left = dfs(root.left, lo, root.val)
-            right = dfs(root.right, root.val, hi)
-            return left and right
+
+            prev = root.val 
+            right = dfs(root.right)
         
-        return dfs(root, float('-inf'), float('inf'))
+            return left and right
+
+        return dfs(root)
+
+        # def dfs(root, lo, hi):
+        #     if root is None: 
+        #         return True
+            
+        #     if root.val <= lo or root.val >= hi: 
+        #         return False
+            
+        #     left = dfs(root.left, lo, root.val)
+        #     right = dfs(root.right, root.val, hi)
+        #     return left and right
+        
+        # return dfs(root, float('-inf'), float('inf'))
 
             
             
