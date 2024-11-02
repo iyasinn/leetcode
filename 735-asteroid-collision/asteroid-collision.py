5a599dfc5ffc127bef2b57d5f0165827c2b373c4
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-02 18:45:39 -0400

    Time: 6 ms (65.60%) | Memory: 17.8 MB (77.43%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,20 +1,11 @@
 
-# assume a comes before b
-def willCollide(a, b):
-    aSign = a // abs(a)
-    bSign = b // abs(b)
-    return aSign == 1 and bSign == -1
-
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
 
         stack = []
-
         for n in asteroids: 
-
             destroyed = False
-
-            while not destroyed and stack and willCollide(stack[-1], n):
+            while not destroyed and stack and stack[-1] > 0 and n < 0:
                 destroyed = bool(abs(stack[-1]) >= abs(n))
                 if abs(stack[-1]) <= abs(n):
                     stack.pop()
