1de03e1b3c0884382467e9f03b47330e7f570738
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-02 23:27:38 -0400

    Time: 35 ms (70.79%) | Memory: 16.9 MB (54.84%) - LeetSync

--- 487-max-consecutive-ones-ii/max-consecutive-ones-ii.py
+++ 487-max-consecutive-ones-ii/max-consecutive-ones-ii.py
@@ -4,7 +4,14 @@ class Solution:
 
         l = 0
         zero = 0
+        """
 
+        Create a sliding window
+        Try to get max
+        If we exceed constraint, try drecreasing window
+
+
+        """
         for r in range(len(nums)):
             zero += int(nums[r] == 0)
             if zero > 1:
