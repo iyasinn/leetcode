a2bdada51a9f762b69eacd23bfd328a6df920701
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-03 14:45:34 -0500

    Time: 35 ms (72.37%) | Memory: 17 MB (99.38%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -1,46 +1,61 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-"""
 
-"""
-
-This feels like a euler tour problem
-1 -> 2 -> 3 -> 4 -> 5
-
-Inorder traversal gets us the euelr tour we need
-
-"""
 
 class Solution:
     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if root is None: 
             return None
 
-        eulerPath = []
+        head = None
+        prev = None
 
         def dfs(root):
-            nonlocal eulerPath
+            nonlocal head
+            nonlocal prev
             if root is None: 
-                return
+                return None, None
 
+            
             dfs(root.left)
-            eulerPath.append(root)
+
+            if not head:
+                head = root
+            else:
+                root.left = prev
+                prev.right = root 
+            
+            prev = root
             dfs(root.right)
-        
+
         dfs(root)
+        head.left = prev
+        prev.right = head
+        return head
 
-        head = eulerPath[0]
-        for i, node in enumerate(eulerPath):
-            nextNode = eulerPath[i + 1] if i + 1 < len(eulerPath) else eulerPath[0]
-            prevNode = eulerPath[i - 1] if i - 1 >= 0 else eulerPath[-1]
+# class Solution:
+#     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+#         if root is None: 
+#             return None
 
-            node.left = prevNode
-            node.right = nextNode
+#         eulerPath = []
+
+#         def dfs(root):
+#             nonlocal eulerPath
+#             if root is None: 
+#                 return
+
+#             dfs(root.left)
+#             eulerPath.append(root)
+#             dfs(root.right)
+        
+#         dfs(root)
+
+#         head = eulerPath[0]
+#         for i, node in enumerate(eulerPath):
+#             nextNode = eulerPath[i + 1] if i + 1 < len(eulerPath) else eulerPath[0]
+#             prevNode = eulerPath[i - 1] if i - 1 >= 0 else eulerPath[-1]
+
+#             node.left = prevNode
+#             node.right = nextNode
 
         return head
 
