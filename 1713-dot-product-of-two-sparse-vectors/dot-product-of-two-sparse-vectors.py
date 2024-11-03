aadc11802b289fee377699a08f7e6d0dff4c9321
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-03 18:39:32 -0500

    Time: 1272 ms (76.05%) | Memory: 21 MB (35.03%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -1,14 +1,19 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
         self.nums = nums
+        self.indices = set([i for i in range(len(nums)) if nums[i] > 0])
+
         # Since it's mostly 0s, what if we stored indices that arent 0 then in O(1) time used those
         
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
 
+
+        indices = set(self.indices).intersection(vec.indices)
         total = 0
-        for i in range(len(vec.nums)):
+        
+        for i in indices:
             total += (self.nums[i] * vec.nums[i])
         return total
         
