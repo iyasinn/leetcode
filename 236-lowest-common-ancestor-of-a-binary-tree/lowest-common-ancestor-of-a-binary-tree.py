57061c7d5aef70e4fe9c9a7bc5921833cb1de68a
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-03 15:24:00 -0500

    Time: 58 ms (16.64%) | Memory: 23 MB (5.28%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,14 +7,59 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root is None:
-            return None
 
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+        self.euler = []
+        self.pIndex = None
+        self.qIndex = None
+        
+        def dfs(root, depth):
+            if root is None: 
+                return
+            
+            dfs(root.left, depth + 1)
 
-        # So root is the ancestor, and we find something in left or right
-        if root in [p, q] or (left and right):
-            return root
-        return left or right
+            self.euler.append((root, depth))
+
+            if p == root: 
+                self.pIndex = len(self.euler) - 1
+            elif q == root:
+                self.qIndex = len(self.euler) - 1
+
+            dfs(root.right, depth + 1)
+
+        dfs(root, 0)
+        pI, qI = min(self.pIndex, self.qIndex), max(self.pIndex, self.qIndex)
+
+        bestDepth, ans = float('inf'), None
+        while pI <= qI:
+            val, depth = self.euler[pI]
+            if depth < bestDepth:
+                ans = val
+                bestDepth = depth
+            pI += 1
+        
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if root is None:
+        #     return None
+
+        # left = self.lowestCommonAncestor(root.left, p, q)
+        # right = self.lowestCommonAncestor(root.right, p, q)
+
+        # # So root is the ancestor, and we find something in left or right
+        # if root in [p, q] or (left and right):
+        #     return root
+        # return left or right
         
