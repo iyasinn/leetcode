743785d92eca65ce0790fc6de04aec63dda7145a
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-02 23:05:12 -0400

    Time: 27 ms (84.10%) | Memory: 17.4 MB (5.25%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -29,6 +29,7 @@ class Solution:
                 if nums[left]==0:k+=1
                 left+=1
         return len(nums)-left
+        
 # class Solution:
 #     def longestOnes(self, nums: List[int], k: int) -> int:
 
