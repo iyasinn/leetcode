2db8b41a1f36f1f6514efe3a3286264fa84ebf3e
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-03 15:58:15 -0500

    Time: 2 ms (7.71%) | Memory: 16.8 MB (5.21%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,32 @@
+"""
+
+So for left pointer
+We can keep going up the left side
+We keep as long as left <= r
+
+"""
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+
+        l = len(nums) - 2
+        while l >= 0 and nums[l] >= nums[l + 1]:
+            l -= 1
+
+        if l < 0:
+            nums.reverse()
+            return
+
+        r = l + 1
+
+        for i in range(l + 1, len(nums)):
+            if nums[i] > nums[l] and nums[i] <= nums[r]:
+                r = i
+        
+        print(r, l)
+        nums[l], nums[r] = nums[r], nums[l]
+        nums[l + 1:] = list((nums[l+1:]))[::-1]
+        
+        
+        
+
