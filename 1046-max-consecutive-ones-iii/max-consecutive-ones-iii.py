b5ac239a737ab6e6cff06164fcea6e42af58f9ca
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-02 23:04:14 -0400

    Time: 29 ms (81.17%) | Memory: 17.4 MB (5.25%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -21,23 +21,34 @@ WHenever you see 1s and 0s, think prefix sum
 """
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
+        left=0
+        for index, i in enumerate(nums):
+            if i==0:
+                k-=1
+            if k<0:
+                if nums[left]==0:k+=1
+                left+=1
+        return len(nums)-left
+# class Solution:
+#     def longestOnes(self, nums: List[int], k: int) -> int:
 
-        # prefix = accumulate([0] + nums)
+        # # prefix = accumulate([0] + nums)
 
 
-        best = 0
-        l = 0 
-        zero = 0
-        
-        for r in range(len(nums)):
-            zero += int(nums[r] == 0)
-            while l <= r and zero > k:
-                zero = zero - 1 if nums[l] == 0 else zero
-                l += 1
-            best = max(best, r - l + 1)
-            r += 1
+        # best = 0
+        # l = 0 
+        # zero = 0
 
-        return best
+        # for r in range(len(nums)):
+        #     if nums[r] == 0: 
+        #         r = 
+        #     if zero > k:
+        #         zero = zero - 1 if nums[l] == 0 else zero
+        #         l += 1
+        #     best = max(best, r - l + 1)
+        #     r += 1
+
+        # return best
 
             
 
