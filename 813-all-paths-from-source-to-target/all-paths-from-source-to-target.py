4735f589bf83d43f7869af54ec16632596e76150
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-04 13:29:09 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (94.50%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -6,22 +6,19 @@ It's directed and acycicl so we dont need a visited matrix
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
 
-        curr = []
-        output = []
+        self.curr = [0]
+        self.output = []
 
         def dfs(i):
-            nonlocal curr
-            nonlocal output
             if i == len(graph) - 1:
-                output.append(curr.copy())
+                self.output.append(self.curr.copy())
                 return
 
             for n in graph[i]:
-                curr.append(n)
+                self.curr.append(n)
                 dfs(n)
-                curr.pop()
+                self.curr.pop()
         
-        curr = [0]
         dfs(0)
 
-        return output
+        return self.output
