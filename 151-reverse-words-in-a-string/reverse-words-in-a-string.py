fef02018ca292b656afd24bb7266d2446a96639a
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-04 01:49:51 -0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (70.83%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -5,9 +5,9 @@ class Solution:
         l = 0
         r = len(s) - 1
 
-        while l <= r:
+        while l < r:
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
-            
+
         return " ".join(s)
