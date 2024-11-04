3f853fa206a242023b36de1ad4593b1a3b4278ba
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-04 13:16:36 -0500

    Time: 301 ms (91.42%) | Memory: 84.3 MB (98.04%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -6,18 +6,54 @@ class Solution:
             adj[a].append(b)
             adj[b].append(a)
 
-        visited = set([source])
+        visited = set()
         dfs = deque([source])
 
         while dfs: 
             top = dfs.popleft()
             if top == destination:
                 return True
+            if top in visited:
+                continue
+
+            visited.add(top)
             
             for other in adj[top]:
-                if other in visited:
-                    continue
-                visited.add(other)
                 dfs.append(other)
 
         return False
+
+
+
+# class Solution:
+#     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+
+#         adj = [[] for _ in range(n)]
+#         # bidirectional
+#         for a, b in edges:
+#             adj[a].append(b)
+#             adj[b].append(a)
+
+#         visited = set()
+#         dfs = [source]
+
+#         while dfs: 
+#             top = dfs.pop()
+#             visited.add(top)
+#             if top == destination:
+#                 return True
+            
+#             for other in adj[top]:
+#                 if other in visited:
+#                     continue
+#                 dfs.append(other)
+
+
+
+
+
+#         return False
+
+
+        
+        
