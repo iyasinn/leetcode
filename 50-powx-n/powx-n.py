70a42a32cb8e199eeb93226bbabcbe8cc8bc8354
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-03 21:07:39 -0500

    Time: 0 ms (100.00%) | Memory: 16.5 MB (71.30%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -27,15 +27,12 @@ x^n = x * x^(n/2) * x^(n/2)
 class Solution:
 
     def myPow(self, x: float, n: int) -> float:
-        if n < 0: 
-            n = n * -1
-            x = 1 / x
+        if n == 0: 
+            return 1
+        elif n < 0: 
+            return self.myPow(1 / x, n * -1)
+        curr = self.myPow(x * x, n // 2)
+        return curr * (1 if n % 2 == 0 else x)
 
-        def dfs(x, n):
-            if n == 0: 
-                return 1
-            curr = self.myPow(x * x, n // 2)
-            return curr * (1 if n % 2 == 0 else x)
 
-        return dfs(x, n)
             
