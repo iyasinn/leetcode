3d496ae0972eba5237343fca08a8d4388a0c0854
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-03 21:25:04 -0500

    Time: 39 ms (65.74%) | Memory: 16.9 MB (87.33%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,45 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+"""
+I don't want to move backwards
+But an easy approach to this is that we only move to large neighbors
+
+
+"""
+
+from typing import Optional
+class Solution:
+
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+
+        self.visited = {None: None}
+
+        def clone(node):
+            if node is None:
+                return
+            if node in self.visited:
+                return
+
+            copy = Node(node.val)
+            self.visited[node] = copy
+
+            for n in node.neighbors:
+                if n in self.visited:
+                    copy.neighbors.append(self.visited[n])
+                else:
+                    clone(n)
+                    copy.neighbors.append(self.visited[n])
+            
+        
+        clone(node)
+        return self.visited[node]
+
+
+
+        
