0be6549e690a55f76f5d7fea4620a851df3a8dac
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-04 12:44:58 -0500

    Time: 303 ms (90.99%) | Memory: 84.2 MB (98.04%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -24,5 +24,36 @@ class Solution:
         return False
 
 
+
+# class Solution:
+#     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+
+#         adj = [[] for _ in range(n)]
+#         # bidirectional
+#         for a, b in edges:
+#             adj[a].append(b)
+#             adj[b].append(a)
+
+#         visited = set()
+#         dfs = [source]
+
+#         while dfs: 
+#             top = dfs.pop()
+#             visited.add(top)
+#             if top == destination:
+#                 return True
+            
+#             for other in adj[top]:
+#                 if other in visited:
+#                     continue
+#                 dfs.append(other)
+
+
+
+
+
+#         return False
+
+
         
         
