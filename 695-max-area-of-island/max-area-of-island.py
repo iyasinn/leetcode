7bc6157cc7df860bb41dad779bd06860a5bf814e
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-04 18:25:15 -0500

    Time: 23 ms (62.58%) | Memory: 17.5 MB (37.97%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
+
+        best = 0
+
+        def sink(r, c):
+            if not (0 <= r < len(grid) and 0 <= c < len(grid[0]) and not visited[r][c]):
+                return 0
+            if grid[r][c] == 0:
+                return 0
+
+            visited[r][c] = True
+
+            total = 1
+            total += sink(r - 1, c)
+            total += sink(r + 1, c)
+            total += sink(r, c - 1)
+            total += sink(r, c + 1)
+
+            return total
+
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                best = max(best, sink(r, c))
+
+        return best
