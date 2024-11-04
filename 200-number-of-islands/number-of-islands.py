f23ec88274b7f83b46f65be22eb670b1ef5e37bd
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-04 18:26:26 -0500

    Time: 241 ms (59.14%) | Memory: 19 MB (71.03%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,15 +1,15 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = [[False] * len(grid[0]) for _ in range(len(grid))]
 
         count = 0
 
         def sink(r, c):
-            if not (0 <= r < len(grid) and 0 <= c < len(grid[0]) and not visited[r][c]):
+            if not (0 <= r < len(grid) and 0 <= c < len(grid[0])):
                 return
             if grid[r][c] == "0":
                 return
-            visited[r][c] = True
+            grid[r][c] = "0"
+
             sink(r - 1, c)
             sink(r + 1, c)
             sink(r, c - 1)
@@ -18,7 +18,7 @@ class Solution:
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if not visited[r][c] and grid[r][c] == "1":
+                if grid[r][c] == "1":
                     count += 1
                     sink(r, c)
 
