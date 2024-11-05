52c3ea6d5edbe6795a72ecdf5dfbbd1384bdb02c
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-04 19:32:41 -0500

    Time: 3 ms (82.36%) | Memory: 16.6 MB (41.66%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -6,13 +6,16 @@ class Solution:
         m = len(grid[0])
 
         visited = [[-1] * m for _ in range(n)]
+        fresh = 0
 
         for r in range(n):
             for c in range(m): 
                 if grid[r][c] == 2:
                     visited[r][c] = 0
                     bfs.append((r, c, 0))
-        
+                if grid[r][c] == 1:
+                    fresh += 1
+        best = 0
         while bfs:
             r, c, time = bfs.popleft()
             for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
@@ -21,16 +24,12 @@ class Solution:
                     continue
                 visited[nr][nc] = time + 1
                 bfs.append((nr, nc, time + 1))
+                fresh -= 1
+                best = max(time + 1, best)
+        
+        return best if fresh == 0 else -1
+    
 
-        print(visited)
-        best = 0
-        for r in range(n):
-            for c in range(m): 
-                if grid[r][c] == 1 and visited[r][c] == -1:
-                    return -1
-                elif grid[r][c] == 1:
-                    best = max(visited[r][c], best)
-        return best
 
             
 
