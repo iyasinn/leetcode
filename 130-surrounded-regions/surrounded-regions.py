2245a75b4156aafc1e9af6ba02cf8104cec5adeb
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-04 22:29:36 -0500

    Time: 7 ms (63.93%) | Memory: 20.9 MB (25.97%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -6,17 +6,15 @@ class Solution:
         n = len(board)
         m = len(board[0])
 
-        visited = set()
-
         def dfs(r, c):
             if not (0 <= r < n and 0 <= c < m):
                 return
             if board[r][c] == 'X':
                 return
-            if (r, c) in visited:
+            if board[r][c] == 'N':
                 return
 
-            visited.add((r, c))
+            board[r][c] = "N"
 
             for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 dfs(r + dx, c + dy)
@@ -25,16 +23,20 @@ class Solution:
         for i in range(n):
             dfs(i, 0)
             dfs(i, m - 1)
-            
+
         # top and bottom border
         for i in range(m):
             dfs(0, i)
             dfs(n - 1, i)
         
+        print(board)
         for r in range(n):
             for c in range(m):
-                if (r, c) not in visited:
-                    board[r][c] = 'X'
+                if board[r][c] == "N":
+                    board[r][c] = "O"
+                elif board[r][c] == "O":
+                    board[r][c] = "X"
+        
     
 
 
