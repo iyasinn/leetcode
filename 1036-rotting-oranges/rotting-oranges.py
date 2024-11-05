d43346fb216de3cbb2f6e106decbdb48dadfb9a1
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-11-04 19:31:39 -0500

    Time: 5 ms (38.61%) | Memory: 16.7 MB (13.57%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,44 +1,36 @@
-def bfs(row, col, oranges, visited, currentTime): 
-    if not (0 <= row < len(oranges) and 0 <= col < len(oranges[0])):
-        return 
-    if (oranges[row][col] != 1): 
-        return 
-
-    if (currentTime >= visited[row][col] and visited[row][col] != -1):
-        return 
-
-    visited[row][col] = currentTime
-
-    bfs(row + 1, col, oranges, visited, currentTime + 1)
-    bfs(row - 1, col, oranges, visited, currentTime + 1)
-    bfs(row, col + 1, oranges, visited, currentTime + 1)
-    bfs(row, col - 1, oranges, visited, currentTime + 1)
-
-# CHECK OUT MULIT SOURCE BFS
 class Solution:
-    def orangesRotting(self, oranges: List[List[int]]) -> int:
-
-        # O(n^2)    
-        visited = [[-1] * len(oranges[0]) for _ in range(len(oranges))] 
-
-        # O(n^2)
-        for row in range(len(oranges)): 
-            for col in range(len(oranges[0])): 
-                if (oranges[row][col] == 2):
-                    bfs(row + 1, col, oranges, visited, 1)
-                    bfs(row - 1, col, oranges, visited, 1)
-                    bfs(row, col + 1, oranges, visited, 1)
-                    bfs(row, col - 1, oranges, visited, 1)
+    def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        time = 0
+        bfs = deque()
+        n = len(grid)
+        m = len(grid[0])
 
-        # O(n^2)
-        for row in range(len(oranges)):
-            for col in range(len(oranges[0])):
-                if (oranges[row][col] == 1 and visited[row][col] == -1):
+        visited = [[-1] * m for _ in range(n)]
+
+        for r in range(n):
+            for c in range(m): 
+                if grid[r][c] == 2:
+                    visited[r][c] = 0
+                    bfs.append((r, c, 0))
+        
+        while bfs:
+            r, c, time = bfs.popleft()
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nr, nc = r + dx, c + dy
+                if not (0 <= nr < n and 0 <= nc < m) or visited[nr][nc] != -1 or grid[nr][nc] == 0:
+                    continue
+                visited[nr][nc] = time + 1
+                bfs.append((nr, nc, time + 1))
+
+        print(visited)
+        best = 0
+        for r in range(n):
+            for c in range(m): 
+                if grid[r][c] == 1 and visited[r][c] == -1:
                     return -1
-                if (oranges[row][col] == 1): 
-                    time = max(time, visited[row][col])
-        
-        return time
-        
+                elif grid[r][c] == 1:
+                    best = max(visited[r][c], best)
+        return best
+
+            
+
