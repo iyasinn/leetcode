53a757b009caf43c8da928bc62d121419d2c5876
Ihsaan Ahmed Yasin <68300319+iyasinn@users.noreply.github.com>
2024-12-22 19:48:28 -0500

    Time: 34 ms (49.19%) | Memory: 73.5 MB (36.90%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -5,12 +5,13 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> set; // int name
 
+        
+
         for (int i = 0; i < nums.size(); i++) {
 
             // cout << "Looking for element: " << nums[i] << endl;
-            auto it = set.find(nums[i]);
 
-            if (it == set.end()) {
+            if (!set.count(nums[i])){
                 // cout << "Inserting " << nums[i] << " into the set " << endl;
                 set.insert(nums[i]);
             }
